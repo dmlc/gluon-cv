b69b332b56632a15dde92d6bc2a5b28c44669eeb
Joshua Zhang <cheungchih@gmail.com>
2018-04-12 22:20:43 -0700

    ignore eval division by 0

--- gluonvision/utils/metrics/voc_detection.py
+++ gluonvision/utils/metrics/voc_detection.py
@@ -190,7 +190,8 @@ class VOCMApMetric(mx.metric.EvalMetric):
 
             # If an element of fp + tp is 0,
             # the corresponding element of prec[l] is nan.
-            prec[l] = tp / (fp + tp)
+            with np.errstate(divide='ignore',invalid='ignore'):
+                prec[l] = tp / (fp + tp)
             # If n_pos[l] is 0, rec[l] is None.
             if self._n_pos[l] > 0:
                 rec[l] = tp / self._n_pos[l]
