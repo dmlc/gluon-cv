6435684bb5d1c822e69a00a8a6f84d21639fd474
Joshua Zhang <cheungchih@gmail.com>
2018-04-15 15:53:05 -0700

    update docs

--- gluonvision/model_zoo/model_store.py
+++ gluonvision/model_zoo/model_store.py
@@ -9,7 +9,7 @@ from ..utils import download
 
 _model_sha1 = {name: checksum for checksum, name in [
     ('4fa2e1ad96b8c8d1ba9e5a43556cd909d70b3985', 'vgg16_atrous'),
-    ('eb8928a2336a9dea56152c785d461f9f04f601fe', 'ssd_300_vgg16_atrous_voc'),
+    ('0e169fbb64efdee6985c3c175ec4298c4bda0298', 'ssd_300_vgg16_atrous_voc'),
     ('13e0a68fa29b46db37a2361475d52a52ef4ff48b', 'ssd_512_vgg16_atrous_voc'),
     ('4a9f39c407e76bbd81348a8bc4f50b2d9ac682ca', 'ssd_512_resnet50_v1_voc')]}
 
