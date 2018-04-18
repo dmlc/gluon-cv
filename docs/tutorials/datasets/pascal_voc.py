e8d38b9a0b4f64c33a8a7dfd221c9473c03dabc0
muli <muli@cs.cmu.edu>
2018-04-18 21:33:07 +0000

    fix XXX

--- docs/tutorials/datasets/pascal_voc.py
+++ docs/tutorials/datasets/pascal_voc.py
@@ -94,6 +94,7 @@ plt.show()
 
 
 ##################################################################
-# Finally, to use both ``train_dataset`` and ``val_dataset`` for training,
-# we can pass them without data transformations and the batch size into
-# :py:class:`gluonvions.data.DetectionDataLoader`, see XXX for an example.
+# Finally, to use both ``train_dataset`` and ``val_dataset`` for training, we
+# can pass them without data transformations and the batch size into
+# :py:class:`gluonvions.data.DetectionDataLoader`, see :download:`train_ssd.py
+# <../../scripts/detection/train_ssd.py>` for an example.
