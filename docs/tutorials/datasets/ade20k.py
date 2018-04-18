b417b69ac36d3fe95e23d4414f0906ec1e8d49ec
Zhang <zhang.hang@rutgers.edu>
2018-04-18 14:49:12 -0700

    ade20k img show

--- docs/tutorials/datasets/ade20k.py
+++ docs/tutorials/datasets/ade20k.py
@@ -12,8 +12,11 @@ categories.
 Prepare the dataset
 -------------------
 
-The easiest way is simply running this script, which will automatically download
-and extract the data into ``~/.mxnet/datasets/ade``.
+The easiest way is simply running this script:
+
+:download:`Download ADE20K Prepare Script: ade20k.py<../../scripts/datasets/ade20k.py>`
+
+which will automatically download and extract the data into ``~/.mxnet/datasets/ade``.
 
 .. code-block:: bash
 
@@ -49,57 +52,31 @@ How to load the dataset
 
 .. code-block:: python
 
-    from gluonvision.data import ADE20KSegmentation
-    train_dataset = ADE20KSegmentation(split='train')
-    val_dataset = ADE20KSegmentation(split='test')
-    print('Training images:', len(train_dataset))
-    print('Validation images:', len(val_dataset))
-
-Dive deep into source code
---------------------------
-
-The implementation of ade20k.py is straightforward. It simply downloads and
-extract the data.
 """
-import os
-import shutil
-import argparse
-import zipfile
-from gluonvision.utils import download, makedirs
 
-_TARGET_DIR = os.path.expanduser('~/.mxnet/datasets/ade')
+################################################################
+# How to load the dataset
+# -----------------------
+#
+# Load image and label from ADE20K is quite straight-forward
 
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description='Initialize ADE20K dataset.',
-        epilog='Example: python setup_ade20k.py',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--download-dir', default=None, help='dataset directory on disk')
-    args = parser.parse_args()
-    return args
-
-def download_ade(path, overwrite=False):
-    _AUG_DOWNLOAD_URLS = [
-        ('http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip', '219e1696abb36c8ba3a3afe7fb2f4b4606a897c7'),
-        ('http://data.csail.mit.edu/places/ADEchallenge/release_test.zip', 'e05747892219d10e9243933371a497e905a4860c'),]
-    download_dir = os.path.join(path, 'downloads')
-    makedirs(download_dir)
-    for url, checksum in _AUG_DOWNLOAD_URLS:
-        filename = download(url, path=download_dir, overwrite=overwrite, sha1_hash=checksum)
-        # extract
-        with zipfile.ZipFile(filename,"r") as zip_ref:
-            zip_ref.extractall(path=path)
+from gluonvision.data import ADE20KSegmentation
+train_dataset = ADE20KSegmentation(split='train')
+val_dataset = ADE20KSegmentation(split='val')
+print('Training images:', len(train_dataset))
+print('Validation images:', len(val_dataset))
 
 
-if __name__ == '__main__':
-    args = parse_args()
-    makedirs(os.path.expanduser('~/.mxnet/datasets'))
-    if args.download_dir is not None:
-        if os.path.isdir(_TARGET_DIR):
-            os.remove(_TARGET_DIR)
-        # make symlink
-        os.symlink(args.download_dir, _TARGET_DIR)
-    else:
-        download_ade(_TARGET_DIR, overwrite=False)
-
+################################################################
+# Visualize the data
+# ------------------
+#
+img, mask = val_dataset[0]
+img.show()
+print(img)
+print(mask)
+# get pallete for the mask and show
+import numpy as np
+from gluonvision.utils.viz import get_color_pallete
+mask = get_color_pallete(np.array(mask), dataset='ade20k')
+mask.show()
