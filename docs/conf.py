aa39a5bd4376554249853cb9eb568e774de7caa7
Joshua Zhang <cheungchih@gmail.com>
2018-04-23 20:56:25 -0700

    mark expected failure

--- docs/conf.py
+++ docs/conf.py
@@ -83,6 +83,7 @@ sphinx_gallery_conf = {
         'gluonvision': None,
     },
     'backreferences_dir': False,
+    'expected_failing_examples': ['tutorials/datasets/ade20k.py']
 }
 
 
--- gluonvision/utils/viz/segmentation.py
+++ gluonvision/utils/viz/segmentation.py
@@ -25,7 +25,7 @@ def get_color_pallete(npimg, dataset='pascal_voc'):
     if dataset == 'pascal_voc' or dataset == 'pascal_aug':
         npimg[npimg == -1] = 255
     # put colormap
-    out_img = Image.fromarray(npimg.astype('uint8').squeeze())
+    out_img = Image.fromarray(npimg.astype('uint8'))
     if dataset == 'ade20k':
         out_img.putpalette(adepallete)
     else:
