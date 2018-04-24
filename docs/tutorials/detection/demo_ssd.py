9300bb502a3af0b74792cbad3b35ac2c6555456b
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2018-04-23 19:25:04 -0700

    detection (#56)

--- docs/tutorials/detection/demo_ssd.py
+++ docs/tutorials/detection/demo_ssd.py
@@ -1,22 +1,23 @@
 """Predict with pre-trained SSD models
 ===================================
 
-This article goes through how to play with pre-trained SSD models with several
+This article shows how to play with pre-trained SSD models with only a few
 lines of code.
 
+First let's import some necessary libraries:
 """
 
 from gluonvision import model_zoo, data, utils
 from matplotlib import pyplot as plt
 
 ######################################################################
-# Obtain a pretrained model
+# Load a pretrained model
 # -------------------------
 #
-# Let's get a SSD model that is trained with 512x512 images on the Pascal VOC
+# Let's get an SSD model trained with 512x512 images on Pascal VOC
 # dataset with ResNet-50 V1 as the base model. By specifying
 # ``pretrained=True``, it will automatically download the model from the model
-# zoo if necessary. For more pretrained models, refer to
+# zoo if necessary. For more pretrained models, please refer to
 # :doc:`../../model_zoo/index`.
 
 net = model_zoo.get_model('ssd_512_resnet50_v1_voc', pretrained=True)
@@ -26,16 +27,16 @@ net = model_zoo.get_model('ssd_512_resnet50_v1_voc', pretrained=True)
 # --------------------
 #
 # Next we download an image, and pre-process with preset data transforms. Here we
-# specify that we resize the short edge of the image into 512 px. But you can
-# feed an arbitrary size image.
+# specify that we resize the short edge of the image to 512 px. But you can
+# feed an arbitrarily sized image.
 #
-# You can provide a list of image filenames, such as ``[im_fname1, im_fname2,
+# You can provide a list of image file names, such as ``[im_fname1, im_fname2,
 # ...]`` to :py:func:`gluonvision.data.transforms.presets.ssd.load_test` if you
 # want to load multiple image together.
 #
-# This function returns two results, the first is a NDArray with shape
-# `(batch_size, RGB_channels, height, width)` that can be feed into the
-# model directly. While the second one contains the images in numpy format to
+# This function returns two results. The first is a NDArray with shape
+# `(batch_size, RGB_channels, height, width)`. It can be fed into the
+# model directly. The second one contains the images in numpy format to
 # easy to be plotted. Since we only loaded a single image, the first dimension
 # of `x` is 1.
 
@@ -48,13 +49,13 @@ print('Shape of pre-processed image:', x.shape)
 # Inference and display
 # ---------------------
 #
-# The forward function will return all possible bounding boxes, and the
-# corresponding predicted class IDs and confidence score. Their shapes are
+# The forward function will return all detected bounding boxes, and the
+# corresponding predicted class IDs and confidence scores. Their shapes are
 # `(batch_size, num_bboxes, 1)`, `(batch_size, num_bboxes, 1)`, and
 # `(batch_size, num_bboxes, 4)`, respectively.
 #
 # We can use :py:func:`gluonvision.utils.viz.plot_bbox` to visualize the
-# results. We slice the results for the first image, then feed into `plot_bbox`.
+# results. We slice the results for the first image and feed them into `plot_bbox`:
 
 class_IDs, scores, bounding_boxs = net(x)
 
