23b5863627ea1643494f409c0199056e2608bbfb
Junyuan Xie <eric.jy.xie@gmail.com>
2018-04-24 14:26:57 -0700

    datasets revision

--- docs/tutorials/datasets/imagenet.py
+++ docs/tutorials/datasets/imagenet.py
@@ -3,29 +3,27 @@
 
 The `ImageNet <http://www.image-net.org/>`_ project contains millions of images
 and thounds of objects for image classification. It is widely used in the
-research community to demonstrate if new proposed models are be able to achieve
-the state-of-the-art results.
+research community for benchmarking state-of-the-art models.
 
 .. image:: https://www.fanyeong.com/wp-content/uploads/2018/01/v2-718f95df083b2d715ee29b018d9eb5c2_r.jpg
    :width: 500 px
 
-The dataset are multiple versions. The commonly used one for image
-classification is the dataset provided in `ILSVRC 2012
+The dataset has multiple versions. The one commonly used for image
+classification is `ILSVRC 2012
 <http://www.image-net.org/challenges/LSVRC/2012/>`_. This tutorial will go
-through the steps of preparing this dataset to be used by GluonCV.
+through the steps of preparing this dataset for GluonCV.
 
 .. note::
 
    You need at least 300 GB disk space to download and extract the dataset. SSD
-   (Solid-state disks) is prefered over HDD because of the better performance
-   on reading and writing small objects (images).
+   (Solid-state disks) is prefered over HDD because of faster speed.
 
-Download the Dataset
---------------------
+Download
+--------
 
-First to go to the `download page <http://www.image-net.org/download-images>`_
-(you may need to register an account), and then find the download link for
-ILSVRC2012. Next go to the download page to download the following two files:
+First, go to the `download page <http://www.image-net.org/download-images>`_
+(you may need to register an account), and find the page for
+ILSVRC2012. Next, find and download the following two files:
 
 ======================== ======
 Filename                 Size
@@ -34,36 +32,35 @@ ILSVRC2012_img_train.tar 138 GB
 ILSVRC2012_img_val.tar   6.3 GB
 ======================== ======
 
-Preprocess the Dataset
-----------------------
+Setup
+-----
 
-Assume the two tar files are downloaded in the folder ``~/ILSVRC2012``. We can use the
+First, please download the helper script
+:download:`imagenet.py<../../../scripts/datasets/imagenet.py>`
+validation image info :download:`imagenet_val_maps.pklz<../../../scripts/datasets/imagenet_val_maps.pklz>`.
+Make sure to put them in the same directory.
+
+Assuming the tar files are saved in folder ``~/ILSVRC2012``. We can use the
 following command to prepare the dataset automatically.
 
 .. code-block:: bash
 
-   # assume your current working directory is git cloned gluon-cv
-   python scripts/datasets/imagenet.py --download-dir ~/ILSVRC2012
-
-.. hint::
-
-   You can download the python script :download:`imagenet.py<../../../scripts/datasets/imagenet.py>`
-   and subdirectory information for 50000 validation images: :download:`imagenet_val_maps.pklz<../../../scripts/datasets/imagenet_val_maps.pklz>`.
-   Put the pklz file in the same directory as the python script.
+   python imagenet.py --download-dir ~/ILSVRC2012
 
 .. note::
 
-   Extracting the images may take tens of minutes to a few hours. E.g., it takes
-   about 30min on AWS EBS General Purpose SSD (gp2).
+   Extracting the images may take a while. For example, it takes
+   about 30min on an AWS EC2 instance with EBS.
 
-In default it will extract the images into ``~/.mxnet/datasets/imagenet``. You
-can specify a different place by using ``--target-dir``.
+By default ``imagenet.py`` will extract the images into
+``~/.mxnet/datasets/imagenet``. You
+can specify a different target folder by setting ``--target-dir``.
 
 
-How to Read the Dataset
------------------------
+Read with GluonCV
+-----------------
 
-The prepared dataset can be loaded by :py:class:`gluoncv.data.ImageNet`
+The prepared dataset can be loaded with utility class :py:class:`gluoncv.data.ImageNet`
 directly. Here is an example that randomly reads 128 images each time and
 performs randomized resizing and cropping.
 """
@@ -86,12 +83,12 @@ train_data = DataLoader(
 
 #########################################################################
 for x, y in train_data:
-    print((x.shape, y.shape))
+    print(x.shape, y.shape)
     break
 
 
 #########################################################################
-# Peek some validation images
+# Plot some validation images
 from gluoncv.utils import viz
 val_dataset = ImageNet(train=False)
 viz.plot_image(val_dataset[1234][0])  # index 0 is image, 1 is label
