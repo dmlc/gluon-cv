290fedf963b6b04668d87155ce1fa9dc4845f0f0
Tong He <hetong007@gmail.com>
2018-04-25 10:45:19 -0700

    fix imagenet demo

--- scripts/classification/imagenet/demo_imagenet.py
+++ scripts/classification/imagenet/demo_imagenet.py
@@ -42,6 +42,7 @@ transform_fn = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
+img = transform_fn(img)
 pred = net(img.expand_dims(0))
 
 topK = 5
