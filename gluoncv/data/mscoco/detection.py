4d571d1e15dca32b7de8b9aef52e8f6852a8a4af
Joshua Zhang <cheungchih@gmail.com>
2018-04-27 15:54:34 -0700

    add utils to allow runtime install pycocotools

---
+++ gluoncv/data/mscoco/detection.py
@@ -0,0 +1,13 @@
+"""MS COCO object detection dataset."""
+from __future__ import absolute_import
+from __future__ import division
+import os
+import logging
+import numpy as np
+import mxnet as mx
+from ..base import VisionDataset
+
+
+class COCODetection(VisionDataset):
+    def __init__(self):
+        pass
