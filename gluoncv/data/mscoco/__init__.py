a8405809de86d361d5149195e6cdb16724b303b1
Joshua Zhang <cheungchih@gmail.com>
2018-04-27 20:07:12 -0700

    mscoco

--- gluoncv/data/mscoco/__init__.py
+++ gluoncv/data/mscoco/__init__.py
@@ -1,29 +1,2 @@
 """MS COCO dataset."""
-# NOTE: for developers
-# please do not import any pycocotools in __init__ because we are trying to lazy
-# import pycocotools to avoid install it for other users who may not use it.
-# only import when you actually use it
 from __future__ import absolute_import
-
-from ...utils.filesystem import import_try_install
-
-def _try_import_pycocotools():
-    """Tricks to optinally install and import pycocotools"""
-    # first we can try import pycocotools
-    try:
-        import pycocotools
-    except ImportError:
-        import os
-        # we need to install pycootools, which is a bit tricky
-        # pycocotools sdist requires Cython, numpy
-        import_try_install('numpy')
-        import_try_install('cython')
-        # pypi pycocotools is not compatible with windows
-        win_url = 'git+https://github.com/zhreshold/cocoapi.git#subdirectory=PythonAPI'
-        try:
-            if os.name == 'nt':
-                import_try_install('pycocotools', win_url)
-            else:
-                import_try_install('pycocotools')
-        except ImportError:
-            raise ImportError('Cannot import or install pycocotools, please refer to FAQ.')
--- gluoncv/data/mscoco/detection.py
+++ gluoncv/data/mscoco/detection.py
@@ -5,9 +5,18 @@ import os
 import logging
 import numpy as np
 import mxnet as mx
+from .utils import try_import_pycocotools
 from ..base import VisionDataset
+
+__all__ = ['COCODetection']
 
 
 class COCODetection(VisionDataset):
-    def __init__(self):
-        pass
+    def __init__(self, root=os.path.join('~', '.mxnet', 'datasets', 'coco'),
+                 splits=('coco_2017_test')):
+        super(COCODetection, self).__init__(root)
+        self._root = os.path.expanduser(root)
+        # lazy import pycocotools
+        try_import_pycocotools()
+        from pycocotools.coco import COCO
+        
