03ad647491e14f656a3162ab65317fc56b516217
Zhang <zhang.hang@rutgers.edu>
2018-05-02 21:29:21 -0700

    unitest for resnetv1b and segmentation models

--- tests/unittests/test_model_zoo.py
+++ tests/unittests/test_model_zoo.py
@@ -49,6 +49,27 @@ def test_ssd_models():
             net.initialize()
         net(x)
 
+def test_resnet_v1b():
+    x = mx.random.uniform(shape=(2, 3, 224, 224))
+    models = ['resnet18_v1b', 'resnet34_v1b', 'resnet50_v1b',
+              'resnet101_v1b', 'resnet152_v1b']
+    for model in models:
+        net = gcv.model_zoo.get_model(model)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            net.initialize()
+        net(x)
+
+def test_segmentation_models():
+    x = mx.random.uniform(shape=(2, 3, 480, 480))
+    models = ['fcn_resnet50_voc', 'fcn_resnet101_voc']
+    for model in models:
+        net = gcv.model_zoo.get_model(model)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            net.initialize()
+        net(x)
+
 if __name__ == '__main__':
     import nose
     nose.runmodule()
