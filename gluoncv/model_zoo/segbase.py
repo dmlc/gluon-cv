c587dd3099c644eb08436be88175846bff4fb805
Hang Zhang <8041160+zhanghang1989@users.noreply.github.com>
2018-05-10 17:36:52 -0700

    fix multi-size testing using single gpu (#110)

--- gluoncv/model_zoo/segbase.py
+++ gluoncv/model_zoo/segbase.py
@@ -156,7 +156,8 @@ class MultiEvalModel(object):
         self.evalmodule = SegEvalModel(module)
 
     def parallel_forward(self, inputs):
-        inputs = [x.as_in_context(ctx) for (x, ctx) in zip(inputs, self.ctx_list)]
+        inputs = tuple([tuple([x.as_in_context(ctx)])
+                        for (x, ctx) in zip(inputs, self.ctx_list)])
         if len(self.ctx_list) == 1:
             return self(*inputs[0])
         return parallel_apply(self, inputs, sync=True)
