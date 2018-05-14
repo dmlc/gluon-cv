f0679a9527ec0e88787bfe891a997d8aa6a72345
Hang Zhang <8041160+zhanghang1989@users.noreply.github.com>
2018-05-14 16:37:08 -0700

    Faster RCNN model params (#119)
    params

--- gluoncv/model_zoo/faster_rcnn/rcnn.py
+++ gluoncv/model_zoo/faster_rcnn/rcnn.py
@@ -1,9 +1,10 @@
 """RCNN Base Model"""
 from mxnet import gluon
+import mxnet.ndarray as F
 import mxnet.gluon.nn as nn
 
 from ..resnetv1b import resnet50_v1b, resnet101_v1b, resnet152_v1b
-# pylint: disable=unused-argument
+# pylint: disable=unused-argument, invalid-sequence-index
 
 class RCNN_ResNet(gluon.Block):
     """RCNN Base model"""
@@ -68,3 +69,25 @@ class RCNN_ResNet(gluon.Block):
         f_cls = self.conv_cls(c4)
         f_reg = self.conv_reg(c4)
         return f_cls, f_reg
+
+    @staticmethod
+    def rcnn_nms(rcnn_cls, bbox_pred, thresh=0.5, pre_nms_topN=-1, topK=100):
+        """RCNN NMS"""
+        nclass = rcnn_cls.shape[1]
+        nboxs = bbox_pred.shape[0]
+        ids = F.concatenate([classid * F.ones((nboxs, 1), rcnn_cls.context)
+                             for classid in range(nclass)], axis=1)
+        rcnn_cls = rcnn_cls.reshape(nboxs * nclass, 1)
+        bbox_pred = bbox_pred.reshape(nboxs * nclass, 4)
+        data = F.concat(ids.reshape(nboxs * nclass, 1), rcnn_cls, bbox_pred, dim=1)
+        nms_pred = F.contrib.box_nms(data, thresh, pre_nms_topN, coord_start=2,
+                                     score_index=1, id_index=0, force_suppress=False)
+        # topK with effective rois
+        effect = int(F.sum(nms_pred[:, 0] >= 0).asscalar())
+        topK = topK if effect > topK else effect
+        # N,1
+        rcnn_ids = nms_pred[:topK, 0]
+        rcnn_scores = nms_pred[:topK, 1]
+        # N, 4
+        rcnn_bbox = nms_pred[:topK, 2:]
+        return rcnn_ids, rcnn_scores, rcnn_bbox
