c118d2ce6b05c6f98c9683b94d9e45d3e16c06b4
Tong He <hetong007@gmail.com>
2018-05-16 15:04:17 -0700

    add senet controls, add recursive visit function

--- gluoncv/utils/__init__.py
+++ gluoncv/utils/__init__.py
@@ -5,7 +5,7 @@ from .download import download
 from .filesystem import makedirs
 from . import bbox
 from .bbox import bbox_iou
-from .block import set_lr_mult
+from .block import recursive_visit, set_lr_mult, freeze_bn
 from . import viz
 from . import random
 from . import metrics
--- gluoncv/utils/block.py
+++ gluoncv/utils/block.py
@@ -1,6 +1,21 @@
 """Utility functions for gluon parameters."""
 import re
+from mxnet.gluon.nn import BatchNorm
 
+def recursive_visit(net, callback, **kwargs):
+    """Recursively visit and apply callback to a net and its sub-net
+
+    Parameters
+    ----------
+    net : mxnet.gluon.Block
+        The network to recursively visit
+    callback : function
+        The callback function to apply to each net block.
+        Its first argument needs to be the block
+    """
+    callback(net, **kwargs)
+    for _, child in net._children.items():
+        recursive_visit(child, callback, **kwargs)
 
 def set_lr_mult(net, pattern, mult=1.0, verbose=False):
     """Reset lr_mult to new value for all parameters that match :obj:`pattern`
@@ -29,3 +44,25 @@ def set_lr_mult(net, pattern, mult=1.0, verbose=False):
         value.lr_mult = mult
         if verbose:
             print("Set lr_mult of {} to {}".format(value.name, mult))
+
+def _freeze_bn_callback(net, use_global_stats=True):
+    if isinstance(net, BatchNorm):
+        net._kwargs['use_global_stats'] = use_global_stats
+
+def freeze_bn(net, use_global_stats=True):
+    """Freeze BatchNorm layers by setting `use_global_stats` to `True`
+
+    Parameters
+    ----------
+    net : mxnet.gluon.Block
+        The network whose BatchNorm layers are going to be modified
+    use_global_stats : bool
+        The value of `use_global_stats` to set for all BatchNorm layers
+
+    Returns
+    ------
+    mxnet.gluon.Block
+        Original network with BatchNorm layers modified.
+
+    """
+    recursive_visit(net, _freeze_bn_callback, use_global_stats=use_global_stats)
