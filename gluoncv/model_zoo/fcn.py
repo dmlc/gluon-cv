a185ab933a96a4da6c08166782f03ead342d84f4
Zhang <zhang.hang@rutgers.edu>
2018-05-16 11:11:37 -0700

    reset context

--- gluoncv/model_zoo/fcn.py
+++ gluoncv/model_zoo/fcn.py
@@ -118,7 +118,8 @@ def get_fcn(dataset='pascal_voc', backbone='resnet50', pretrained=False,
         from .model_store import get_model_file
         model.load_params(get_model_file('fcn_%s_%s'%(backbone, acronyms[dataset]),
                                          root=root))
-    return model.collect_params().reset_ctx(ctx)
+    model.collect_params().reset_ctx(ctx)
+    return model
 
 def get_fcn_voc_resnet50(**kwargs):
     r"""FCN model with base network ResNet-50 pre-trained on Pascal VOC dataset
