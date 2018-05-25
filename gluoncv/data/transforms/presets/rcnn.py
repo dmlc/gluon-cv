4f332906626553854c85e193ed137c6f34652322
Joshua Z. Zhang <cheungchih@gmail.com>
2018-05-24 17:59:19 -0700

    add script

--- gluoncv/data/transforms/presets/rcnn.py
+++ gluoncv/data/transforms/presets/rcnn.py
@@ -6,7 +6,53 @@ from .. import bbox as tbbox
 from .. import image as timage
 from .. import experimental
 
-__all__ = ['load_test', ]
+__all__ = ['load_test', 'FasterRCNNDefaultTrainTransform', 'FasterRCNNDefaultValTransform']
+
+def load_test(filenames, short, max_size=1024, mean=(0.485, 0.456, 0.406),
+              std=(0.229, 0.224, 0.225)):
+    """A util function to load all images, transform them to tensor by applying
+    normalizations. This function support 1 filename or list of filenames.
+
+    Parameters
+    ----------
+    filenames : str or list of str
+        Image filename(s) to be loaded.
+    short : int
+        Resize image short side to this `short` and keep aspect ratio.
+    max_size : int, optional
+        Maximum longer side length to fit image.
+        This is to limit the input image shape, avoid processing too large image.
+    mean : iterable of float
+        Mean pixel values.
+    std : iterable of float
+        Standard deviations of pixel values.
+
+    Returns
+    -------
+    (mxnet.NDArray, numpy.ndarray) or list of such tuple
+        A (1, 3, H, W) mxnet NDArray as input to network, and a numpy ndarray as
+        original un-normalized color image for display.
+        If multiple image names are supplied, return two lists. You can use
+        `zip()`` to collapse it.
+
+    """
+    if isinstance(filenames, str):
+        filenames = [filenames]
+    tensors = []
+    origs = []
+    for f in filenames:
+        img = mx.image.imread(f)
+        img = mx.image.resize_short(img, short)
+        if isinstance(max_size, int) and max(img.shape) > max_size:
+            img = timage.resize_long(img, max_size)
+        orig_img = img.asnumpy().astype('uint8')
+        img = mx.nd.image.to_tensor(img)
+        img = mx.nd.image.normalize(img, mean=mean, std=std)
+        tensors.append(img.expand_dims(0))
+        origs.append(orig_img)
+    if len(tensors) == 1:
+        return tensors[0], origs[0]
+    return tensors, origs
 
 
 class FasterRCNNDefaultTrainTransform(object):
@@ -83,18 +129,11 @@ class FasterRCNNDefaultValTransform(object):
         Standard deviation to be divided from image. Default is [0.229, 0.224, 0.225].
 
     """
-    def __init__(self, width, height, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
-        self._width = width
-        self._height = height
+    def __init__(self, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
         self._mean = mean
         self._std = std
 
     def __call__(self, src, label):
-        # resize
-        h, w, _ = src.shape
-        img = timage.imresize(src, self._width, self._height)
-        bbox = tbbox.resize(label, in_size=(w, h), out_size=(self._width, self._height))
-
-        img = mx.nd.image.to_tensor(img)
+        img = mx.nd.image.to_tensor(src)
         img = mx.nd.image.normalize(img, mean=self._mean, std=self._std)
-        return img, bbox.astype('float32')
+        return img, label.astype('float32')
