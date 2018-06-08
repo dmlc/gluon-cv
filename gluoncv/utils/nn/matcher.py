031120879f06c5fd67a68f1b92556908b00eea17
Joshua Z. Zhang <cheungchih@gmail.com>
2018-06-07 17:53:46 -0700

    fix

--- gluoncv/utils/nn/matcher.py
+++ gluoncv/utils/nn/matcher.py
@@ -98,7 +98,7 @@ class BipartiteMatcherV1(gluon.HybridBlock):
         pargmax = x.argmax(axis=-1, keepdims=True)  # (B, num_anchor, 1)
         maxs = x.max(axis=-2, keepdims=True)  # (B, 1, num_gt)
         pmax = F.pick(x, pargmax, axis=-1, keepdims=True)   # (B, num_anchor, 1)
-        mask = F.broadcast_greater(pmax + self._eps, maxs)  # (B, num_anchor, num_gt)
+        mask = F.broadcast_greater_equal(pmax + self._eps, maxs)  # (B, num_anchor, num_gt)
         mask = F.pick(mask, pargmax, axis=-1, keepdims=True)  # (B, num_anchor, 1)
         new_match = F.where(mask > 0, pargmax, F.ones_like(pargmax) * -1)
         result = F.where(match[0] < 0, new_match.squeeze(axis=-1), match[0])
