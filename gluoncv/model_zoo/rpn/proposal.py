626871cabcfa3e865c0461350116cca0f160c6e0
Joshua Z. Zhang <cheungchih@gmail.com>
2018-06-11 16:12:29 -0700

    ignore out of bound anchors

--- gluoncv/model_zoo/rpn/proposal.py
+++ gluoncv/model_zoo/rpn/proposal.py
@@ -67,9 +67,16 @@ class RPNProposal(gluon.HybridBlock):
 
             # remove bounding boxes that don't meet the min_size constraint
             # by setting them to (-1, -1, -1, -1)
-            width = roi.slice_axis(axis=-1, begin=2, end=3)
-            height = roi.slice_axis(axis=-1, begin=3, end=None)
-            invalid = (width < self._min_size) + (height < self._min_size)
+            # width = roi.slice_axis(axis=-1, begin=2, end=3)
+            # height = roi.slice_axis(axis=-1, begin=3, end=None)
+            # invalid = (width < self._min_size) + (height < self._min_size)
+
+            # remove out of bound anchors
+            axmin, aymin, axmax, aymax = F.split(anchor, axis=-1, num_outputs=4)
+            # it's a bit tricky to get right/bottom boundary in hybridblock
+            wrange = F.arange(0, 2560).reshape((1, 1, 1, 2560)).slice_like(img, axes=(3)).max().reshape((1, 1, 1))
+            hrange = F.arange(0, 2560).reshape((1, 1, 2560, 1)).slice_like(img, axes=(2)).max().reshape((1, 1, 1))
+            invalid = (axmin < 0) + (aymin < 0) + F.broadcast_greater(axmax, wrange) + F.broadcast_greater(aymax, hrange)
             score = F.where(invalid, F.zeros_like(invalid), score)
             invalid = F.repeat(invalid, axis=-1, repeats=4)
             roi = F.where(invalid, F.ones_like(invalid) * -1, roi)
