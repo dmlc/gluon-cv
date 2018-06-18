97406c2e0a0e34376a2a702cc2f6b941c02f694c
Tong He <hetong007@gmail.com>
2018-06-18 15:33:13 -0700

    download files for tutorial

--- tests/unittests/test_model_zoo.py
+++ tests/unittests/test_model_zoo.py
@@ -26,6 +26,8 @@ import numpy as np
 import gluoncv as gcv
 from common import try_gpu, with_cpu
 
+from gluoncv.utils import download
+
 @with_cpu(0)
 def _test_model_list(model_list, ctx, x, **kwargs):
     for model in model_list:
@@ -49,6 +51,14 @@ def _test_model_list(model_list, ctx, x, **kwargs):
             mx.nd.waitall()
 
 def test_classification_models():
+    download('https://raw.githubusercontent.com/hetong007/gluon-cv/temp_files/temp_folder/train.idx',
+             '~/.mxnet/datasets/imagenet/rec/train.idx')
+    download('https://raw.githubusercontent.com/hetong007/gluon-cv/temp_files/temp_folder/train.rec',
+             '~/.mxnet/datasets/imagenet/rec/train.rec')
+    download('https://raw.githubusercontent.com/hetong007/gluon-cv/temp_files/temp_folder/val.idx',
+             '~/.mxnet/datasets/imagenet/rec/val.idx')
+    download('https://raw.githubusercontent.com/hetong007/gluon-cv/temp_files/temp_folder/val.rec',
+             '~/.mxnet/datasets/imagenet/rec/val.rec')
     ctx = mx.context.current_context()
     x = mx.random.uniform(shape=(2, 3, 32, 32), ctx=ctx)
     cifar_models = [
