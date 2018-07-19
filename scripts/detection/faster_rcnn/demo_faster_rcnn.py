da90426f0e8ba77c99a3c87b9216e64a8ad01a8a
Jian Guo <ijkguo@gmail.com>
2018-07-19 16:21:02 -0700

    fix demo: output now has batch dim

--- scripts/detection/faster_rcnn/demo_faster_rcnn.py
+++ scripts/detection/faster_rcnn/demo_faster_rcnn.py
@@ -38,9 +38,9 @@ if __name__ == '__main__':
 
     # grab some image if not specified
     if not args.images.strip():
-        gcv.utils.download("https://cloud.githubusercontent.com/assets/3307514/" +
-            "20012568/cbc2d6f6-a27d-11e6-94c3-d35a9cb47609.jpg", 'street.jpg')
-        image_list = ['street.jpg']
+        gcv.utils.download('https://github.com/dmlc/web-data/blob/master/' +
+                           'gluoncv/detection/biking.jpg?raw=true', 'biking.jpg')
+        image_list = ['biking.jpg']
     else:
         image_list = [x.strip() for x in args.images.split(',') if x.strip()]
 
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     ax = None
     for image in image_list:
         x, img = presets.rcnn.load_test(image, short=args.short, max_size=args.max_size)
-        ids, scores, bboxes = [xx.asnumpy() for xx in net(x)]
+        ids, scores, bboxes = [xx[0].asnumpy() for xx in net(x)]
         ax = gcv.utils.viz.plot_bbox(img, bboxes, scores, ids,
                                      class_names=net.classes, ax=ax)
         plt.show()
