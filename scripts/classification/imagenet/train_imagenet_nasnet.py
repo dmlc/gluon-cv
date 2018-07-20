f0c4c9673cbb34dd768b7b7efdb7e0fbe6b1f478
Tong He <hetong007@gmail.com>
2018-07-20 15:01:31 -0700

    update training script

--- scripts/classification/imagenet/train_imagenet_nasnet.py
+++ scripts/classification/imagenet/train_imagenet_nasnet.py
@@ -1,10 +1,6 @@
-import matplotlib
-matplotlib.use('Agg')
-
-import argparse, time, logging
+import argparse, time, logging, os
 
 import mxnet as mx
-import numpy as np
 from mxnet import gluon, nd
 from mxnet import autograd as ag
 from mxnet.gluon import nn
@@ -13,16 +9,26 @@ from mxnet.gluon.data.vision import transforms
 from gluoncv.data import imagenet
 from gluoncv.loss import SoftmaxCrossEntropyLossWithAux
 from gluoncv.model_zoo import get_model
-from gluoncv.utils import makedirs, TrainingHistory, freeze_bn
+from gluoncv.utils import makedirs, LRScheduler
 
 # CLI
 parser = argparse.ArgumentParser(description='Train a model for image classification.')
 parser.add_argument('--data-dir', type=str, default='~/.mxnet/datasets/imagenet',
                     help='training and validation pictures to use.')
-parser.add_argument('--dummy', action='store_true',
-                    help='use dummy data to test training speed. default is false.')
+parser.add_argument('--rec-train', type=str, default='~/.mxnet/datasets/imagenet/rec/train.rec',
+                    help='the training data')
+parser.add_argument('--rec-train-idx', type=str, default='~/.mxnet/datasets/imagenet/rec/train.idx',
+                    help='the index of training data')
+parser.add_argument('--rec-val', type=str, default='~/.mxnet/datasets/imagenet/rec/val.rec',
+                    help='the validation data')
+parser.add_argument('--rec-val-idx', type=str, default='~/.mxnet/datasets/imagenet/rec/val.idx',
+                    help='the index of validation data')
+parser.add_argument('--use-rec', action='store_true',
+                    help='use image record iter for data input. default is false.')
 parser.add_argument('--batch-size', type=int, default=32,
                     help='training batch size per device (CPU/GPU).')
+parser.add_argument('--dtype', type=str, default='float32',
+                    help='data type for training. default is float32')
 parser.add_argument('--num-gpus', type=int, default=0,
                     help='number of gpus to use.')
 parser.add_argument('-j', '--num-data-workers', dest='num_workers', default=4, type=int,
@@ -35,6 +41,8 @@ parser.add_argument('--momentum', type=float, default=0.9,
                     help='momentum value for optimizer, default is 0.9.')
 parser.add_argument('--wd', type=float, default=0.0001,
                     help='weight decay rate. default is 0.0001.')
+parser.add_argument('--lr-mode', type=str, default='step',
+                    help='learning rate scheduler mode. options are step, poly and cosine.')
 parser.add_argument('--lr-decay', type=float, default=0.1,
                     help='decay rate of learning rate. default is 0.1.')
 parser.add_argument('--lr-decay-period', type=int, default=0,
@@ -51,8 +59,6 @@ parser.add_argument('--use_se', action='store_true',
                     help='use SE layers or not in resnext. default is false.')
 parser.add_argument('--label-smoothing', action='store_true',
                     help='use label smoothing or not in training. default is false.')
-parser.add_argument('--freeze-bn', action='store_true',
-                    help='freeze batchnorm layers in the last frew training epochs or not. default is false.')
 parser.add_argument('--batch-norm', action='store_true',
                     help='enable batch normalization or not in vgg. default is false.')
 parser.add_argument('--use-pretrained', action='store_true',
@@ -65,8 +71,6 @@ parser.add_argument('--save-dir', type=str, default='params',
                     help='directory of saved models')
 parser.add_argument('--logging-dir', type=str, default='logs',
                     help='directory of training logs')
-parser.add_argument('--save-plot-dir', type=str, default='.',
-                    help='the path to save the history plot')
 opt = parser.parse_args()
 
 logging.basicConfig(level=logging.INFO)
@@ -74,6 +78,7 @@ logging.info(opt)
 
 batch_size = opt.batch_size
 classes = 1000
+num_training_samples = 1281167
 
 num_gpus = opt.num_gpus
 batch_size *= max(1, num_gpus)
@@ -82,7 +87,15 @@ num_workers = opt.num_workers
 
 lr_decay = opt.lr_decay
 lr_decay_period = opt.lr_decay_period
-lr_decay_epoch = [int(i) for i in opt.lr_decay_epoch.split(',')] + [np.inf]
+if opt.lr_decay_period > 0:
+    lr_decay_epoch = list(range(lr_decay_period, opt.num_epochs, lr_decay_period))
+else:
+    lr_decay_epoch = [int(i) for i in opt.lr_decay_epoch.split(',')]
+num_batches = num_training_samples // batch_size
+lr_scheduler = LRScheduler(mode=opt.lr_mode, baselr=opt.lr,
+                            niters=num_batches, nepochs=opt.num_epochs,
+                            step=lr_decay_epoch, step_factor=opt.lr_decay, power=2,
+                            warmup_epochs=opt.warmup_epochs)
 
 model_name = opt.model
 
@@ -93,18 +106,120 @@ elif model_name.startswith('resnext'):
     kwargs['use_se'] = opt.use_se
 
 optimizer = 'nag'
-optimizer_params = {'learning_rate': opt.lr, 'wd': opt.wd, 'momentum': opt.momentum}
+optimizer_params = {'wd': opt.wd, 'momentum': opt.momentum, 'lr_scheduler': lr_scheduler}
+if opt.dtype != 'float32':
+    optimizer_params['multi_precision'] = True
 
 net = get_model(model_name, **kwargs)
-if opt.use_se:
-    model_name = 'se_' + model_name
+net.cast(opt.dtype)
+
+# Two functions for reading data from record file or raw images
+def get_data_rec(rec_train, rec_train_idx, rec_val, rec_val_idx, batch_size, num_workers):
+    rec_train = os.path.expanduser(rec_train)
+    rec_train_idx = os.path.expanduser(rec_train_idx)
+    rec_val = os.path.expanduser(rec_val)
+    rec_val_idx = os.path.expanduser(rec_val_idx)
+    jitter_param = 0.4
+    lighting_param = 0.1
+    input_size = opt.input_size
+    mean_rgb = [123.68, 116.779, 103.939]
+    std_rgb = [58.393, 57.12, 57.375]
+
+    def batch_fn(batch, ctx):
+        data = gluon.utils.split_and_load(batch.data[0], ctx_list=ctx, batch_axis=0)
+        label = gluon.utils.split_and_load(batch.label[0], ctx_list=ctx, batch_axis=0)
+        return data, label
+
+    train_data = mx.io.ImageRecordIter(
+        path_imgrec         = rec_train,
+        path_imgidx         = rec_train_idx,
+        preprocess_threads  = num_workers,
+        shuffle             = True,
+        batch_size          = batch_size,
+
+        data_shape          = (3, input_size, input_size),
+        mean_r              = mean_rgb[0],
+        mean_g              = mean_rgb[1],
+        mean_b              = mean_rgb[2],
+        std_r               = std_rgb[0],
+        std_g               = std_rgb[1],
+        std_b               = std_rgb[2],
+        rand_mirror         = True,
+        random_resized_crop = True,
+        max_aspect_ratio    = 4. / 3.,
+        min_aspect_ratio    = 3. / 4.,
+        max_random_area     = 1,
+        min_random_area     = 0.08,
+        brightness          = jitter_param,
+        saturation          = jitter_param,
+        contrast            = jitter_param,
+        pca_noise           = lighting_param,
+    )
+    val_data = mx.io.ImageRecordIter(
+        path_imgrec         = rec_val,
+        path_imgidx         = rec_val_idx,
+        preprocess_threads  = num_workers,
+        shuffle             = False,
+        batch_size          = batch_size,
+
+        resize              = 256,
+        data_shape          = (3, input_size, input_size),
+        mean_r              = mean_rgb[0],
+        mean_g              = mean_rgb[1],
+        mean_b              = mean_rgb[2],
+        std_r               = std_rgb[0],
+        std_g               = std_rgb[1],
+        std_b               = std_rgb[2],
+    )
+    return train_data, val_data, batch_fn
+
+def get_data_loader(data_dir, batch_size, num_workers):
+    normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    jitter_param = 0.4
+    lighting_param = 0.1
+    input_size = opt.input_size
+
+    def batch_fn(batch, ctx):
+        data = gluon.utils.split_and_load(batch[0], ctx_list=ctx, batch_axis=0)
+        label = gluon.utils.split_and_load(batch[1], ctx_list=ctx, batch_axis=0)
+        return data, label
+
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(input_size),
+        transforms.RandomFlipLeftRight(),
+        transforms.RandomColorJitter(brightness=jitter_param, contrast=jitter_param,
+                                     saturation=jitter_param),
+        transforms.RandomLighting(lighting_param),
+        transforms.ToTensor(),
+        normalize
+    ])
+    transform_test = transforms.Compose([
+        transforms.Resize(256, keep_ratio=True),
+        transforms.CenterCrop(input_size),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    train_data = gluon.data.DataLoader(
+        imagenet.classification.ImageNet(data_dir, train=True).transform_first(transform_train),
+        batch_size=batch_size, shuffle=True, last_batch='discard', num_workers=num_workers)
+    val_data = gluon.data.DataLoader(
+        imagenet.classification.ImageNet(data_dir, train=False).transform_first(transform_test),
+        batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_data, val_data, batch_fn
+
+if opt.use_rec:
+    train_data, val_data, batch_fn = get_data_rec(opt.rec_train, opt.rec_train_idx,
+                                                  opt.rec_val, opt.rec_val_idx,
+                                                  batch_size, num_workers)
+else:
+    train_data, val_data, batch_fn = get_data_loader(opt.data_dir, batch_size, num_workers)
 
 acc_top1 = mx.metric.Accuracy()
 acc_top5 = mx.metric.TopKAccuracy(5)
 acc_top1_aux = mx.metric.Accuracy()
 acc_top5_aux = mx.metric.TopKAccuracy(5)
-train_history = TrainingHistory(['training-top1-err', 'training-top5-err',
-                                 'validation-top1-err', 'validation-top5-err'])
 
 save_frequency = opt.save_frequency
 if opt.save_dir and save_frequency:
@@ -113,31 +228,6 @@ if opt.save_dir and save_frequency:
 else:
     save_dir = ''
     save_frequency = 0
-
-plot_path = opt.save_plot_dir
-
-normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-jitter_param = 0.0 if model_name.startswith('mobilenet') else 0.4
-lighting_param = 0.0 if model_name.startswith('mobilenet') else 0.1
-
-input_size = opt.input_size
-
-transform_train = transforms.Compose([
-    transforms.RandomResizedCrop(input_size),
-    transforms.RandomFlipLeftRight(),
-    transforms.RandomColorJitter(brightness=jitter_param, contrast=jitter_param,
-                                 saturation=jitter_param),
-    transforms.RandomLighting(lighting_param),
-    transforms.ToTensor(),
-    normalize
-])
-
-transform_test = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(input_size),
-    transforms.ToTensor(),
-    normalize
-])
 
 def smooth(label, classes, eta=0.1):
     if isinstance(label, nd.NDArray):
@@ -152,14 +242,15 @@ def smooth(label, classes, eta=0.1):
     return smoothed
 
 def test(ctx, val_data):
+    if opt.use_rec:
+        val_data.reset()
     acc_top1.reset()
     acc_top5.reset()
     acc_top1_aux.reset()
     acc_top5_aux.reset()
     for i, batch in enumerate(val_data):
-        data = gluon.utils.split_and_load(batch[0], ctx_list=ctx, batch_axis=0)
-        label = gluon.utils.split_and_load(batch[1], ctx_list=ctx, batch_axis=0)
-        outputs = [net(X) for X in data]
+        data, label = batch_fn(batch, ctx)
+        outputs = [net(X.astype(opt.dtype, copy=False)) for X in data]
         acc_top1.update(label, [o[0] for o in outputs])
         acc_top5.update(label, [o[0] for o in outputs])
         acc_top1_aux.update(label, [o[1] for o in outputs])
@@ -171,17 +262,10 @@ def test(ctx, val_data):
     _, top5_aux = acc_top5_aux.get()
     return (1-top1, 1-top5, 1-top1_aux, 1-top5_aux)
 
-def train(epochs, ctx):
+def train(ctx):
     if isinstance(ctx, mx.Context):
         ctx = [ctx]
     net.initialize(mx.init.MSRAPrelu(), ctx=ctx)
-
-    train_data = gluon.data.DataLoader(
-        imagenet.classification.ImageNet(opt.data_dir, train=True).transform_first(transform_train),
-        batch_size=batch_size, shuffle=True, last_batch='discard', num_workers=num_workers)
-    val_data = gluon.data.DataLoader(
-        imagenet.classification.ImageNet(opt.data_dir, train=False).transform_first(transform_test),
-        batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     trainer = gluon.Trainer(net.collect_params(), optimizer, optimizer_params)
     if opt.label_smoothing:
@@ -189,43 +273,36 @@ def train(epochs, ctx):
     else:
         L = SoftmaxCrossEntropyLossWithAux(aux_weight=0.4)
 
-    lr_decay_count = 0
-
     best_val_score = 1
 
-    for epoch in range(epochs):
+    for epoch in range(opt.num_epochs):
         tic = time.time()
+        if opt.use_rec:
+            train_data.reset()
         acc_top1.reset()
         acc_top5.reset()
         acc_top1_aux.reset()
         acc_top5_aux.reset()
         btic = time.time()
-        train_loss = 0
-        num_batch = len(train_data)
-
-        if lr_decay_period and epoch and epoch % lr_decay_period == 0:
-            trainer.set_learning_rate(trainer.learning_rate*lr_decay)
-        elif lr_decay_period == 0 and epoch == lr_decay_epoch[lr_decay_count]:
-            trainer.set_learning_rate(trainer.learning_rate*lr_decay)
-            lr_decay_count += 1
 
         for i, batch in enumerate(train_data):
-            data = gluon.utils.split_and_load(batch[0], ctx_list=ctx, batch_axis=0)
-            label = gluon.utils.split_and_load(batch[1], ctx_list=ctx, batch_axis=0)
+            data, label = batch_fn(batch, ctx)
             if opt.label_smoothing:
                 label_smooth = smooth(label, classes)
             else:
                 label_smooth = label
             with ag.record():
-                outputs = [net(X) for X in data]
+                outputs = [net(X.astype(opt.dtype, copy=False)) for X in data]
                 loss = [L(yhat[0], yhat[1], y) for yhat, y in zip(outputs, label_smooth)]
-            ag.backward(loss)
+            for l in loss:
+                l.backward()
+            lr_scheduler.update(i, epoch)
             trainer.step(batch_size)
+
             acc_top1.update(label, [o[0] for o in outputs])
             acc_top5.update(label, [o[0] for o in outputs])
             acc_top1_aux.update(label, [o[1] for o in outputs])
             acc_top5_aux.update(label, [o[1] for o in outputs])
-            train_loss += sum([l.sum().asscalar() for l in loss])
             if opt.log_interval and not (i+1)%opt.log_interval:
                 _, top1 = acc_top1.get()
                 _, top5 = acc_top5.get()
@@ -243,88 +320,29 @@ def train(epochs, ctx):
         _, top1_aux = acc_top1_aux.get()
         _, top5_aux = acc_top5_aux.get()
         err_top1, err_top5, err_top1_aux, err_top5_aux = (1-top1, 1-top5, 1-top1_aux, 1-top5_aux)
-        train_loss /= num_batch * batch_size
 
         err_top1_val, err_top5_val, err_top1_val_aux, err_top5_val_aux = test(ctx, val_data)
-        train_history.update([err_top1, err_top5, err_top1_val, err_top5_val])
-        train_history.plot(['training-top1-err', 'validation-top1-err'],
-                           save_path='%s/%s_top1.png'%(plot_path, model_name))
-        train_history.plot(['training-top5-err', 'validation-top5-err'],
-                           save_path='%s/%s_top5.png'%(plot_path, model_name))
 
-        logging.info('[Epoch %d] training: err-top1=%f err-top5=%f err-top1_aux=%f err-top5_aux=%f loss=%f'%
-            (epoch, err_top1, err_top5, err_top1_aux, err_top5_aux, train_loss))
+        logging.info('[Epoch %d] training: err-top1=%f err-top5=%f err-top1_aux=%f err-top5_aux=%f'%
+            (epoch, err_top1, err_top5, err_top1_aux, err_top5_aux))
         logging.info('[Epoch %d] time cost: %f'%(epoch, time.time()-tic))
         logging.info('[Epoch %d] validation: err-top1=%f err-top5=%f err-top1_aux=%f err-top5_aux=%f'%
             (epoch, err_top1_val, err_top5_val, err_top1_val_aux, err_top5_val_aux))
 
         if err_top1_val < best_val_score and epoch > 50:
             best_val_score = err_top1_val
-            net.save_params('%s/%.4f-imagenet-%s-%d-best.params'%(save_dir, best_val_score, model_name, epoch))
+            net.save_parameters('%s/%.4f-imagenet-%s-%d-best.params'%(save_dir, best_val_score, model_name, epoch))
 
         if save_frequency and save_dir and (epoch + 1) % save_frequency == 0:
-            net.save_params('%s/imagenet-%s-%d.params'%(save_dir, model_name, epoch))
+            net.save_parameters('%s/imagenet-%s-%d.params'%(save_dir, model_name, epoch))
 
     if save_frequency and save_dir:
-        net.save_params('%s/imagenet-%s-%d.params'%(save_dir, model_name, epochs-1))
-
-def train_dummy(ctx):
-    if isinstance(ctx, mx.Context):
-        ctx = [ctx]
-    net.initialize(mx.init.MSRAPrelu(), ctx=ctx)
-
-    data = []
-    label = []
-    bs = batch_size // len(ctx)
-    for c in ctx:
-        data.append(mx.nd.random.uniform(shape=(bs,3,input_size,input_size), ctx = c))
-        label.append(mx.nd.ones(shape=(bs), ctx = c))
-
-    trainer = gluon.Trainer(net.collect_params(), optimizer, optimizer_params)
-    if opt.label_smoothing:
-        L = SoftmaxCrossEntropyLossWithAux(sparse_label=False, aux_weight=0.4)
-    else:
-        L = SoftmaxCrossEntropyLossWithAux(aux_weight=0.4)
-
-    acc_top1.reset()
-    acc_top5.reset()
-    btic = time.time()
-    train_loss = 0
-    num_batch = 1000
-    warm_up = 100
-
-    for i in range(num_batch):
-        if i == warm_up:
-            tic = time.time()
-        if opt.label_smoothing:
-            label_smooth = smooth(label, classes)
-        else:
-            label_smooth = label
-        with ag.record():
-            outputs = [net(X) for X in data]
-            loss = [L(yhat[0], yhat[1], y) for yhat, y in zip(outputs, label_smooth)]
-        ag.backward(loss)
-        trainer.step(batch_size)
-        acc_top1.update(label, [o[0] for o in outputs])
-        acc_top5.update(label, [o[0] for o in outputs])
-        train_loss += sum([l.sum().asscalar() for l in loss])
-
-        if opt.log_interval and not (i+1)%opt.log_interval:
-            logging.info('Batch [%d]\tSpeed: %f samples/sec'%(
-                         i, batch_size*opt.log_interval/(time.time()-btic)))
-            btic = time.time()
-
-    total_time_cost = time.time()-tic
-    logging.info('Test finished. Average Speed: %f samples/sec. Total time cost: %f'%(
-                 batch_size*(num_batch-warm_up)/total_time_cost, total_time_cost))
+        net.save_parameters('%s/imagenet-%s-%d.params'%(save_dir, model_name, opt.num_epochs-1))
 
 def main():
     if opt.mode == 'hybrid':
-        net.hybridize()
-    if opt.dummy:
-        train_dummy(context)
-    else:
-        train(opt.num_epochs, context)
+        net.hybridize(static_alloc=True, static_shape=True)
+    train(context)
 
 if __name__ == '__main__':
     main()
