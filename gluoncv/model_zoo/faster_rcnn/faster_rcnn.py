851a8f2d8fa1647343e74f3abcbc0bf8f858b54f
Jian Guo <ijkguo@gmail.com>
2018-07-26 15:41:09 -0700

    rpn min size = 0

--- gluoncv/model_zoo/faster_rcnn/faster_rcnn.py
+++ gluoncv/model_zoo/faster_rcnn/faster_rcnn.py
@@ -389,7 +389,7 @@ def faster_rcnn_resnet50_v1b_coco(pretrained=False, pretrained_base=True, **kwar
         rpn_channel=1024, base_size=16, scales=(2, 4, 8, 16, 32),
         ratios=(0.5, 1, 2), alloc_size=(128, 128), rpn_nms_thresh=0.7,
         rpn_train_pre_nms=12000, rpn_train_post_nms=2000,
-        rpn_test_pre_nms=6000, rpn_test_post_nms=1000, rpn_min_size=16,
+        rpn_test_pre_nms=6000, rpn_test_post_nms=1000, rpn_min_size=0,
         num_sample=512, pos_iou_thresh=0.5, pos_ratio=0.25,
         **kwargs)
 
@@ -483,7 +483,7 @@ def faster_rcnn_resnet50_v2a_coco(pretrained=False, pretrained_base=True, **kwar
         rpn_channel=1024, base_size=16, scales=(2, 4, 8, 16, 32),
         ratios=(0.5, 1, 2), alloc_size=(128, 128), rpn_nms_thresh=0.7,
         rpn_train_pre_nms=12000, rpn_train_post_nms=2000,
-        rpn_test_pre_nms=6000, rpn_test_post_nms=1000, rpn_min_size=16,
+        rpn_test_pre_nms=6000, rpn_test_post_nms=1000, rpn_min_size=0,
         num_sample=512, pos_iou_thresh=0.5, pos_ratio=0.25,
         **kwargs)
 
