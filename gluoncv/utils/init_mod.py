a0987dda58a07195e7631fed9a1c86d1561a4e63
abhinavs95 <abhinav0301@gmail.com>
2018-07-26 15:11:11 -0700

    update init_mod

--- gluoncv/utils/init_mod.py
+++ gluoncv/utils/init_mod.py
@@ -15,7 +15,7 @@ def init_mod_sqnet(net, ctx):
     net: Input net with modified initialization
     '''
     data = [mx.nd.zeros(shape=(1, 3, 224, 224), ctx=i) for i in ctx]
-    outputs = [net(X) for X in data]
+    _ = [net(X) for X in data]
     new_init_w = mx.nd.random_normal(shape=(1000, 512, 1, 1), scale=0.01)
     final_conv_layer_params = net.output[0].params
     final_conv_layer_params.get('weight').set_data(new_init_w)
