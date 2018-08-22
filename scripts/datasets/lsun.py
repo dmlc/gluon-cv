8a5b6f4ba1827566238470e9f477ae9dfb9d24ae
陈晓辰 <1092441751@qq.com>
2018-08-22 19:57:59 +0800

    compatible for python3

--- scripts/datasets/lsun.py
+++ scripts/datasets/lsun.py
@@ -7,7 +7,11 @@ import json
 from os.path import join
 
 import subprocess
-import urllib2
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+from gluoncv.utils.download import download
 
 __author__ = 'Fisher Yu'
 __email__ = 'fy@cs.princeton.edu'
@@ -16,11 +20,11 @@ __license__ = 'MIT'
 
 def list_categories(tag):
     url = 'http://lsun.cs.princeton.edu/htbin/list.cgi?tag=' + tag
-    f = urllib2.urlopen(url)
+    f = urlopen(url)
     return json.loads(f.read())
 
 
-def download(out_dir, category, set_name, tag):
+def download_lsun(out_dir, category, set_name, tag):
     url = 'http://lsun.cs.princeton.edu/htbin/download.cgi?tag={tag}' \
           '&category={category}&set={set_name}'.format(**locals())
     if set_name == 'test':
@@ -28,9 +32,7 @@ def download(out_dir, category, set_name, tag):
     else:
         out_name = '{category}_{set_name}_lmdb.zip'.format(**locals())
     out_path = join(out_dir, out_name)
-    cmd = ['curl', url, '-o', out_path]
-    print('Downloading', category, set_name, 'set')
-    subprocess.call(cmd)
+    download(url,out_path,True)
 
 
 def main():
@@ -44,18 +46,18 @@ def main():
     if args.category is None:
         print('Downloading', len(categories), 'categories')
         for category in categories:
-            download(args.out_dir, category, 'train', args.tag)
-            download(args.out_dir, category, 'val', args.tag)
-        download(args.out_dir, '', 'test', args.tag)
+            download_lsun(args.out_dir, category, 'train', args.tag)
+            download_lsun(args.out_dir, category, 'val', args.tag)
+        download_lsun(args.out_dir, '', 'test', args.tag)
     else:
         if args.category == 'test':
-            download(args.out_dir, '', 'test', args.tag)
+            download_lsun(args.out_dir, '', 'test', args.tag)
         elif args.category not in categories:
             print('Error:', args.category, "doesn't exist in",
                   args.tag, 'LSUN release')
         else:
-            download(args.out_dir, args.category, 'train', args.tag)
-            download(args.out_dir, args.category, 'val', args.tag)
+            download_lsun(args.out_dir, args.category, 'train', args.tag)
+            download_lsun(args.out_dir, args.category, 'val', args.tag)
 
 
 if __name__ == '__main__':
