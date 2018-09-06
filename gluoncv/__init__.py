0e33e20c54b90715b9a09e44e466b9947d19010c
Joshua Z. Zhang <cheungchih@gmail.com>
2018-09-06 13:10:52 -0700

    fix lint

--- gluoncv/__init__.py
+++ gluoncv/__init__.py
@@ -4,13 +4,21 @@ from __future__ import absolute_import
 import warnings
 from distutils.version import LooseVersion
 
+__version__ = '0.3.0'
+
+from . import data
+from . import model_zoo
+from . import nn
+from . import utils
+from . import loss
+
+# mxnet version check
 mx_version = '3.3.0'
 try:
-
     import mxnet as mx
     if LooseVersion(mx.__version__) < LooseVersion(mx_version):
         msg = (
-            "Legacy mxnet=={} detected, some new module may not work properly. "
+            "Legacy mxnet=={} detected, some new modules may not work properly. "
             "mxnet>={} is recommended. You can use pip to upgrade mxnet "
             "`pip install mxnet --pre --upgrade`").format(mx.__version__, mx_version)
         warnings.warn(msg)
@@ -19,11 +27,3 @@ except ImportError:
         "Unable to import dependency mxnet. "
         "A quick tip is to install via `pip install mxnet --pre`. "
         "please refer to https://gluon-cv.mxnet.io/#installation for details.")
-
-__version__ = '0.3.0'
-
-from . import data
-from . import model_zoo
-from . import nn
-from . import utils
-from . import loss
