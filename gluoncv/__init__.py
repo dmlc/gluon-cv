58e967efb619c1b62ef0a88a6150e7d4b2aa6c5d
Joshua Z. Zhang <cheungchih@gmail.com>
2018-09-07 15:44:53 -0700

    fix import order

--- gluoncv/__init__.py
+++ gluoncv/__init__.py
@@ -1,21 +1,14 @@
 # coding: utf-8
+# pylint: disable=wrong-import-position
 """GluonCV: a deep learning vision toolkit powered by Gluon."""
 from __future__ import absolute_import
-import warnings
-from distutils.version import LooseVersion
-
-__version__ = '0.3.0'
-
-from . import data
-from . import model_zoo
-from . import nn
-from . import utils
-from . import loss
 
 # mxnet version check
 mx_version = '1.3.0'
 try:
     import mxnet as mx
+    import warnings
+    from distutils.version import LooseVersion
     if LooseVersion(mx.__version__) < LooseVersion(mx_version):
         msg = (
             "Legacy mxnet=={} detected, some new modules may not work properly. "
@@ -27,3 +20,11 @@ except ImportError:
         "Unable to import dependency mxnet. "
         "A quick tip is to install via `pip install mxnet --pre`. "
         "please refer to https://gluon-cv.mxnet.io/#installation for details.")
+
+__version__ = '0.3.0'
+
+from . import data
+from . import model_zoo
+from . import nn
+from . import utils
+from . import loss
