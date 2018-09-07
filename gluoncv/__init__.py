9669d728d17715586765a9f2b1886fc054e1d6ed
Joshua Z. Zhang <cheungchih@gmail.com>
2018-09-07 16:31:11 -0700

    add mxnet version check (#301)
    * add mxnet version check
    
    * fix lint
    
    * correct version
    
    * fix import order

--- gluoncv/__init__.py
+++ gluoncv/__init__.py
@@ -1,6 +1,25 @@
 # coding: utf-8
+# pylint: disable=wrong-import-position
 """GluonCV: a deep learning vision toolkit powered by Gluon."""
 from __future__ import absolute_import
+
+# mxnet version check
+mx_version = '1.3.0'
+try:
+    import mxnet as mx
+    import warnings
+    from distutils.version import LooseVersion
+    if LooseVersion(mx.__version__) < LooseVersion(mx_version):
+        msg = (
+            "Legacy mxnet=={} detected, some new modules may not work properly. "
+            "mxnet>={} is recommended. You can use pip to upgrade mxnet "
+            "`pip install mxnet --pre --upgrade`").format(mx.__version__, mx_version)
+        warnings.warn(msg)
+except ImportError:
+    raise ImportError(
+        "Unable to import dependency mxnet. "
+        "A quick tip is to install via `pip install mxnet --pre`. "
+        "please refer to https://gluon-cv.mxnet.io/#installation for details.")
 
 __version__ = '0.3.0'
 
