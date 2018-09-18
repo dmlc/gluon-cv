cd7a83af6df15b05e85b9a8fce21f8d9aa229c77
Ragav Venkatesan <email@ragav.net>
2018-09-18 14:51:24 -0700

    Allowing uneven split in evaluation. (#317)

--- scripts/segmentation/train.py
+++ scripts/segmentation/train.py
@@ -170,7 +170,7 @@ class Trainer(object):
         for i, (data, target) in enumerate(tbar):
             outputs = self.evaluator(data.astype(args.dtype, copy=False))
             outputs = [x[0] for x in outputs]
-            targets = mx.gluon.utils.split_and_load(target, args.ctx)
+            targets = mx.gluon.utils.split_and_load(target, args.ctx, even_split=False)
             self.metric.update(targets, outputs)
             pixAcc, mIoU = self.metric.get()
             tbar.set_description('Epoch %d, validation pixAcc: %.3f, mIoU: %.3f'%\
