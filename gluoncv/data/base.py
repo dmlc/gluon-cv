37b6dc3f4ad3646b6627ecbcb10a9f01f92c5f4d
Thomas Delteil <thomas.delteil1@gmail.com>
2018-10-11 17:14:10 -0700

    Update base.py

--- gluoncv/data/base.py
+++ gluoncv/data/base.py
@@ -10,7 +10,7 @@ class ClassProperty(object):
     def __get__(self, owner_self, owner_cls):
         return self.fget(owner_cls)
 
-    
+
 class VisionDataset(dataset.Dataset):
     """Base Dataset with directory checker.
 
