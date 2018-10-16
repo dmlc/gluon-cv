f9c6392b3bf7a6d7f6e461e5d8c1423ff45e20dc
Joshua Z. Zhang <cheungchih@gmail.com>
2018-10-16 14:08:42 -0700

    alabaster theme

--- docs/conf.py
+++ docs/conf.py
@@ -126,7 +126,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_logo = '_static/gluon_white.png'
 html_favicon = '_static/gluon_s2.png'
 # html_theme_path = guzzle_sphinx_theme.html_theme_path()
