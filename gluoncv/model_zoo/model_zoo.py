1145115b2a7ea999fa10adab69fe735548c3ae55
yangxv <x.yang@bmi-tech.cn>
2018-10-16 11:20:35 +0800

    add model_zoo

--- gluoncv/model_zoo/model_zoo.py
+++ gluoncv/model_zoo/model_zoo.py
@@ -24,6 +24,7 @@ from .resnet import *
 from .squeezenet import *
 from .vgg import *
 from .mobilenet import *
+from .residual_attentionnet import *
 
 __all__ = ['get_model', 'get_model_list']
 
@@ -74,7 +75,7 @@ _models = {
     'mobilenetv2_0.25': mobilenet_v2_0_25,
     'ssd_300_vgg16_atrous_voc': ssd_300_vgg16_atrous_voc,
     'ssd_300_vgg16_atrous_coco': ssd_300_vgg16_atrous_coco,
-    'ssd_300_vgg16_atrous_custom' : ssd_300_vgg16_atrous_custom,
+    'ssd_300_vgg16_atrous_custom': ssd_300_vgg16_atrous_custom,
     'ssd_512_vgg16_atrous_voc': ssd_512_vgg16_atrous_voc,
     'ssd_512_vgg16_atrous_coco': ssd_512_vgg16_atrous_coco,
     'ssd_512_vgg16_atrous_custom': ssd_512_vgg16_atrous_custom,
@@ -154,7 +155,12 @@ _models = {
     'nasnet_5_1538': nasnet_5_1538,
     'nasnet_7_1920': nasnet_7_1920,
     'nasnet_6_4032': nasnet_6_4032,
-    }
+    'residualattentionnet56': residualattentionnet56,
+    'residualattentionnet92': residualattentionnet92,
+    'residualattentionnet56_32input': residualattentionnet56_32input,
+    'residualattentionnet92_32input': residualattentionnet92_32input
+}
+
 
 def get_model(name, **kwargs):
     """Returns a pre-defined model by name
@@ -185,6 +191,7 @@ def get_model(name, **kwargs):
         raise ValueError(err_str)
     net = _models[name](**kwargs)
     return net
+
 
 def get_model_list():
     """Get the entire list of model names in model_zoo.
