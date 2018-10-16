1bc7ef220e4940f7cd93fe5d733c62545c626c52
yangxv <x.yang@bmi-tech.cn>
2018-10-16 11:09:55 +0800

    fix bugs.

--- gluoncv/model_zoo/residual_attentionnet.py
+++ gluoncv/model_zoo/residual_attentionnet.py
@@ -16,7 +16,7 @@
 # under the License.
 
 # coding: utf-8
-# pylint: disable= arguments-differ,missing-docstring
+# pylint: disable= unused-argument,missing-docstring
 """ResidualAttentionNetwork, implemented in Gluon."""
 
 __all__ = ['ResidualAttentionModel_448input', 'ResidualAttentionModel',
