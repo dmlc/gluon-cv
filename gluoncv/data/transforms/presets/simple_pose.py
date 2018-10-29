d80a271b36089141c58308a3b42da094c502d7ce
Joshua Z. Zhang <cheungchih@gmail.com>
2018-10-29 12:31:50 -0700

    simple pose transform

---
+++ gluoncv/data/transforms/presets/simple_pose.py
@@ -0,0 +1,70 @@
+"""Transforms for simple pose estimation."""
+from __future__ import absolute_import
+
+import random
+import numpy as np
+import mxnet as mx
+from ..image import random_flip as random_flip_image
+from ..pose import flip_joints_3d, get_affine_transform, affine_transform
+from ....model_zoo.simple_pose.pose_target import SimplePoseGaussianTargetGenerator
+from ....utils.filesystem import try_import_cv2
+
+
+class SimplePoseDefaultTrainTransform(object):
+    """Default training transform for simple pose.
+
+    Parameters
+    ----------
+    num_joints : int
+        Number of joints defined by dataset
+    image_size : tuple of int
+        Image size, as (width, height).
+    heatmap_size : tuple of int
+        Heatmap size, as (width, height).
+    sigma : float
+        Gaussian sigma for the heatmap generation.
+
+    """
+    def __init__(self, num_joints, joint_pairs, image_size, heatmap_size, sigma=2,
+                 mean=(0.485, 0.456, 0.406),
+                 std=(0.229, 0.224, 0.225), random_flip=True, scale_factor=0.25, rotation_factor=30,
+                 **kwargs):
+        self._target_generator = SimplePoseGaussianTargetGenerator(
+            num_joints, image_size, heatmap_size, sigma)
+        self._image_size = image_size
+        self._joint_pairs = joint_pairs
+        self._width = image_size[0]
+        self._height = image_size[1]
+        self._mean = mean
+        self._std = std
+        self._random_flip = random_flip
+        self._scale_factor = scale_factor
+        self._rotation_factor = rotation_factor
+
+    def __call__(self, src, label):
+        cv2 = try_import_cv2()
+        joints_3d = label['joints_3d']
+        center = list(label['center'])
+        scale = label['scale']
+        score = label.get('score', 1)
+
+        # rescale
+        sf = self._scale_factor
+        s = s * np.clip(np.random.randn() * sf + 1, 1 - sf, 1 + sf)
+
+        # rotation
+        rf = self._rotation_factor
+        r = np.clip(np.random.randn() * rf, -rf * 2, rf * 2, if random.random() <= 0.6 else 0)
+
+        if self._random_flip:
+            src, fliped = random_flip_image(src, px=0.5, py=0)
+            if fliped[0]:
+                joints = flip_joints_3d(joints_3d, src.shape[1], self._joint_pairs)
+            center[0] = src.shape[1] - center[0] - 1
+
+        trans = get_affine_transform(center, s, r, self._image_size)
+        w, h = self._image_size
+        image = cv2.warpAffine(src, trans, (int(h), int(w)), flags=cv2.INTER_LINEAR)
+
+        # deal with joints visibility
+        for i in range(self)
