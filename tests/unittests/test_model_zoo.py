6e60825db7c77a0b6178c3f13b6e68199fc81410
PistonYang <pistonyang@gmail.com>
2018-11-09 10:26:30 +0800

    fix bugs.

--- tests/unittests/test_model_zoo.py
+++ tests/unittests/test_model_zoo.py
@@ -74,6 +74,7 @@ def test_classification_models():
         'cifar_resnet20_v2', 'cifar_resnet56_v2', 'cifar_resnet110_v2',
         'cifar_wideresnet16_10', 'cifar_wideresnet28_10', 'cifar_wideresnet40_8',
         'cifar_resnext29_32x4d', 'cifar_resnext29_16x64d',
+        'residualattentionnet56_32input', 'residualattentionnet92_32input',
     ]
     _test_model_list(cifar_models, ctx, x)
 
@@ -105,8 +106,7 @@ def test_imagenet_models():
               'darknet53', 'alexnet',
               'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn',
               'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn',
-              'residualattentionnet56', 'residualattentionnet92',
-              'residualattentionnet56_32input', 'residualattentionnet92_32input']
+              'residualattentionnet56', 'residualattentionnet92']
     _test_model_list(models, ctx, x)
 
     # 299x299
