2990bfead4513a8507b37b01f61ca45ef30126ac
Hao Zhang <1339754545@qq.com>
2018-11-26 14:49:51 +0800

    Update mobilenet.py

--- gluoncv/model_zoo/mobilenet.py
+++ gluoncv/model_zoo/mobilenet.py
@@ -234,7 +234,7 @@ def get_mobilenet(multiplier, pretrained=False, ctx=cpu(),
     """
     net = MobileNet(multiplier, num_sync_bn_devices=num_sync_bn_devices, **kwargs)
     if pretrained:
-        from gluoncv.model_zoo.model_store import get_model_file
+        from .model_store import get_model_file
         version_suffix = '{0:.2f}'.format(multiplier)
         if version_suffix in ('1.00', '0.50'):
             version_suffix = version_suffix[:-1]
