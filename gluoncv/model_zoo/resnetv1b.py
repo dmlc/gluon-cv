0b4a664caf144cb1010c9eeb831c161a523e3ac1
Hang Zhang <zhang.hang@rutgers.edu>
2019-02-25 14:52:31 -0800

    lint

--- gluoncv/model_zoo/resnetv1b.py
+++ gluoncv/model_zoo/resnetv1b.py
@@ -61,20 +61,21 @@ class BottleneckV1b(HybridBlock):
                  downsample=None, previous_dilation=1, norm_layer=None,
                  norm_kwargs=None, last_gamma=False, **kwargs):
         super(BottleneckV1b, self).__init__()
+        norm_kwargs = norm_kwargs if norm_kwargs is not None else {}
         self.conv1 = nn.Conv2D(channels=planes, kernel_size=1,
                                use_bias=False)
-        self.bn1 = norm_layer(in_channels=planes, **({} if norm_kwargs is None else norm_kwargs))
+        self.bn1 = norm_layer(in_channels=planes, **norm_kwargs)
         self.relu1 = nn.Activation('relu')
         self.conv2 = nn.Conv2D(channels=planes, kernel_size=3, strides=strides,
                                padding=dilation, dilation=dilation, use_bias=False)
-        self.bn2 = norm_layer(in_channels=planes, **({} if norm_kwargs is None else norm_kwargs))
+        self.bn2 = norm_layer(in_channels=planes, **norm_kwargs)
         self.relu2 = nn.Activation('relu')
         self.conv3 = nn.Conv2D(channels=planes * 4, kernel_size=1, use_bias=False)
         if not last_gamma:
-            self.bn3 = norm_layer(in_channels=planes*4, **({} if norm_kwargs is None else norm_kwargs))
+            self.bn3 = norm_layer(in_channels=planes*4, **norm_kwargs)
         else:
             self.bn3 = norm_layer(in_channels=planes*4, gamma_initializer='zeros',
-                                  **({} if norm_kwargs is None else norm_kwargs))
+                                  **norm_kwargs)
         self.relu3 = nn.Activation('relu')
         self.downsample = downsample
         self.dilation = dilation
@@ -159,16 +160,16 @@ class ResNetV1b(HybridBlock):
                 self.conv1 = nn.HybridSequential(prefix='conv1')
                 self.conv1.add(nn.Conv2D(channels=stem_width, kernel_size=3, strides=2,
                                          padding=1, use_bias=False))
-                self.conv1.add(norm_layer(in_channels=stem_width, **({} if norm_kwargs is None else norm_kwargs)))
+                self.conv1.add(norm_layer(in_channels=stem_width, **norm_kwargs))
                 self.conv1.add(nn.Activation('relu'))
                 self.conv1.add(nn.Conv2D(channels=stem_width, kernel_size=3, strides=1,
                                          padding=1, use_bias=False))
-                self.conv1.add(norm_layer(in_channels=stem_width, **({} if norm_kwargs is None else norm_kwargs)))
+                self.conv1.add(norm_layer(in_channels=stem_width, **norm_kwargs))
                 self.conv1.add(nn.Activation('relu'))
                 self.conv1.add(nn.Conv2D(channels=stem_width*2, kernel_size=3, strides=1,
                                          padding=1, use_bias=False))
             self.bn1 = norm_layer(in_channels=64 if not deep_stem else stem_width*2,
-                                  **({} if norm_kwargs is None else norm_kwargs))
+                                  **norm_kwargs)
             self.relu = nn.Activation('relu')
             self.maxpool = nn.MaxPool2D(pool_size=3, strides=2, padding=1)
             self.layer1 = self._make_layer(1, block, 64, layers[0], avg_down=avg_down,
