388e49e4a9c52bd9ad551d5792e2c50071488d36
Joshua Z. Zhang <cheungchih@gmail.com>
2019-04-18 11:18:28 -0700

    fix lint

--- gluoncv/model_zoo/yolo/yolo_target.py
+++ gluoncv/model_zoo/yolo/yolo_target.py
@@ -268,7 +268,8 @@ class YOLOV3TargetMerger(gluon.HybridBlock):
                 smooth_weight = min(1. / self._num_class, 1. / 40)
                 class_targets = F.where(
                     class_targets > 0.5, class_targets - smooth_weight, class_targets)
-                class_targets = F.where((class_targets < -0.5) + (class_targets > 0.5),
+                class_targets = F.where(
+                    (class_targets < -0.5) + (class_targets > 0.5),
                     class_targets, F.ones_like(class_targets) * smooth_weight)
             class_mask = mask.tile(reps=(self._num_class,)) * (class_targets >= 0)
             return [F.stop_gradient(x) for x in [objectness, center_targets, scale_targets,
