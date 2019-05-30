a990c2f148efccd3f7dc0cc0ccd81c03a0f91dd5
Joshua Z. Zhang <cheungchih@gmail.com>
2019-05-29 18:57:57 -0700

    fix ctx issue of faster/mask-rcnn (#786)
    * fix ctx issue
    
    * fix
    
    * ignore not initialized parameters when reset ctx

--- gluoncv/model_zoo/mask_rcnn/mask_rcnn.py
+++ gluoncv/model_zoo/mask_rcnn/mask_rcnn.py
@@ -329,6 +329,12 @@ def get_mask_rcnn(name, dataset, pretrained=False, ctx=mx.cpu(),
         from ..model_store import get_model_file
         full_name = '_'.join(('mask_rcnn', name, dataset))
         net.load_parameters(get_model_file(full_name, tag=pretrained, root=root), ctx=ctx)
+    else:
+        for v in net.collect_params().values():
+            try:
+                v.reset_ctx(ctx)
+            except ValueError:
+                pass
     return net
 
 
