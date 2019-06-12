6f6b134b61f4525f2ad900e3dba28384b528a37e
Serge Panev <spanev@nvidia.com>
2019-06-12 14:06:27 +0900

    Add Horovod, AMP and DALI to SSD training script
    Signed-off-by: Serge Panev <spanev@nvidia.com>

--- gluoncv/data/transforms/presets/ssd.py
+++ gluoncv/data/transforms/presets/ssd.py
@@ -6,7 +6,16 @@ from .. import bbox as tbbox
 from .. import image as timage
 from .. import experimental
 
-__all__ = ['transform_test', 'load_test', 'SSDDefaultTrainTransform', 'SSDDefaultValTransform']
+try:
+    from nvidia.dali.pipeline import Pipeline
+    import nvidia.dali.ops as ops
+    import nvidia.dali.types as types
+except ImportError:
+    class Pipeline:
+        def __init__(self):
+            raise NotImplementedError("DALI not found, please check if you installed it correctly.")
+
+__all__ = ['transform_test', 'load_test', 'SSDDefaultTrainTransform', 'SSDDefaultValTransform', 'SSDCocoDALIPipeline']
 
 def transform_test(imgs, short, max_size=1024, mean=(0.485, 0.456, 0.406),
                    std=(0.229, 0.224, 0.225)):
@@ -211,3 +220,98 @@ class SSDDefaultValTransform(object):
         img = mx.nd.image.to_tensor(img)
         img = mx.nd.image.normalize(img, mean=self._mean, std=self._std)
         return img, bbox.astype(img.dtype)
+
+class SSDCocoDALIPipeline(Pipeline):
+    def __init__(self, num_shards, device_id, shard_id, batch_size, data_shape, anchors, file_root, annotations_file, num_workers):
+        super(SSDCocoDALIPipeline, self).__init__(
+            batch_size=batch_size,
+            device_id=device_id,
+            num_threads=num_workers)
+
+        self.input = ops.COCOReader(
+            file_root=file_root,
+            annotations_file=annotations_file,
+            skip_empty=True,
+            shard_id=shard_id,
+            num_shards=num_shards,
+            ratio=True,
+            ltrb=True,
+            shuffle_after_epoch=True)
+
+        self.decode = ops.HostDecoder(device="cpu", output_type=types.RGB)
+
+        # Augumentation techniques
+        self.crop = ops.RandomBBoxCrop(
+            device="cpu",
+            aspect_ratio=[0.5, 2.0],
+            thresholds=[0, 0.1, 0.3, 0.5, 0.7, 0.9],
+            scaling=[0.3, 1.0],
+            ltrb=True,
+            allow_no_crop=True,
+            num_attempts=1)
+        self.slice = ops.Slice(device="cpu")
+        self.twist = ops.ColorTwist(device="gpu")
+        self.resize = ops.Resize(
+            device="cpu",
+            resize_x=data_shape,
+            resize_y=data_shape,
+            min_filter=types.DALIInterpType.INTERP_TRIANGULAR)
+
+        # output_dtype = types.FLOAT16 if args.fp16 else types.FLOAT
+        output_dtype = types.FLOAT
+
+        self.normalize = ops.CropMirrorNormalize(
+            device="gpu",
+            crop=(data_shape, data_shape),
+            mean=[0.485 * 255, 0.456 * 255, 0.406 * 255],
+            std=[0.229 * 255, 0.224 * 255, 0.225 * 255],
+            mirror=0,
+            output_dtype=output_dtype,
+            output_layout=types.NCHW,
+            pad_output=False)
+
+        # Random variables
+        self.rng1 = ops.Uniform(range=[0.5, 1.5])
+        self.rng2 = ops.Uniform(range=[0.875, 1.125])
+        self.rng3 = ops.Uniform(range=[-0.5, 0.5])
+
+        self.flip = ops.Flip(device="cpu")
+        self.bbflip = ops.BbFlip(device="cpu", ltrb=True)
+        self.flip_coin = ops.CoinFlip(probability=0.5)
+
+        self.box_encoder = ops.BoxEncoder(
+            device="cpu",
+            criteria=0.5,
+            anchors=anchors,
+            offset=True,
+            stds=[0.1,0.1,0.2,0.2],
+            scale=data_shape)
+
+
+    def define_graph(self):
+        saturation = self.rng1()
+        contrast = self.rng1()
+        brightness = self.rng2()
+        hue = self.rng3()
+        coin_rnd = self.flip_coin()
+
+        inputs, bboxes, labels = self.input(name="Reader")
+        images = self.decode(inputs)
+
+        crop_begin, crop_size, bboxes, labels = self.crop(bboxes, labels)
+        images = self.slice(images, crop_begin, crop_size)
+
+        images = self.flip(images, horizontal=coin_rnd)
+        bboxes = self.bbflip(bboxes, horizontal=coin_rnd)
+        images = self.resize(images)
+        images = images.gpu()
+        images = self.twist(
+            images,
+            saturation=saturation,
+            contrast=contrast,
+            brightness=brightness,
+            hue=hue)
+        images = self.normalize(images)
+        bboxes, labels = self.box_encoder(bboxes, labels)
+
+        return (images, bboxes.gpu(), labels.gpu())
