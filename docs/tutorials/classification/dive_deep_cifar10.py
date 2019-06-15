377cf5a7a3b22c2f297bebd621e777f5301fec55
Tong He <hetong007@gmail.com>
2019-06-14 21:46:46 -0700

    Update dive_deep_cifar10.py (#817)

--- docs/tutorials/classification/dive_deep_cifar10.py
+++ docs/tutorials/classification/dive_deep_cifar10.py
@@ -117,7 +117,6 @@ transform_train = transforms.Compose([
 # function for prediction is:
 
 transform_test = transforms.Compose([
-    transforms.Resize(32),
     transforms.ToTensor(),
     transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
 ])
