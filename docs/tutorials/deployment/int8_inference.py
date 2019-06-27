15287f6d22cabc4acde1b7f734550d4a852c9527
xinyu-intel <xinyu1.chen@intel.com>
2019-06-27 23:26:01 +0800

    add omp setting

--- docs/tutorials/deployment/int8_inference.py
+++ docs/tutorials/deployment/int8_inference.py
@@ -49,6 +49,10 @@ Demo usage for SSD
 
 .. code:: bash
 
+   # set omp to use all physical cores of one socket
+   export KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0
+   export CPUs=`lscpu | grep 'Core(s) per socket' | awk '{print $4}'`
+   export OMP_NUM_THREADS=$(CPUs)
    # with Pascal VOC validation dataset saved on disk
    python eval_ssd.py --network=vgg16_atrous --quantized --data-shape=300 --batch-size=224 --dataset=voc --benchmark
 
