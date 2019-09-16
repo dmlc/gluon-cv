deba33b4709972571c14b21dcfe35ad4785ff22e
Joshua Z. Zhang <cheungchih@gmail.com>
2019-09-16 16:22:09 -0700

    try import horovod (#940)

--- scripts/detection/ssd/train_ssd.py
+++ scripts/detection/ssd/train_ssd.py
@@ -24,7 +24,10 @@ from gluoncv.utils.metrics.accuracy import Accuracy
 
 from mxnet.contrib import amp
 
-import horovod.mxnet as hvd
+try:
+    import horovod.mxnet as hvd
+except ImportError:
+    hvd = None
 
 try:
     from nvidia.dali.plugin.mxnet import DALIGenericIterator
@@ -90,6 +93,8 @@ def parse_args():
                         '--gpus is ignored when using --horovod.')
 
     args = parser.parse_args()
+    if args.horovod:
+        assert hvd, "You are trying to use horovod support but it's not installed"
     return args
 
 def get_dataset(dataset, args):
