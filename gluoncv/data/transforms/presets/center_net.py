c441a13fde2f96c191490752742ecf029c24a576
Joshua Z. Zhang <cheungchih@gmail.com>
2019-10-11 14:02:22 -0700

    debug dataset

--- gluoncv/data/transforms/presets/center_net.py
+++ gluoncv/data/transforms/presets/center_net.py
@@ -164,6 +164,101 @@ class CenterNetDefaultTrainTransform(object):
             img.shape[2], img.shape[1], gt_bboxes, gt_ids)
         return img, heatmap, wh_target, wh_mask, center_reg, center_reg_mask
 
+class CenterNetDefaultTrainTransformDebug(object):
+    """Default SSD training transform which includes tons of image augmentations.
+
+    Parameters
+    ----------
+    width : int
+        Image width.
+    height : int
+        Image height.
+    num_class : int
+        Number of categories
+    scale_factor : int, default is 4
+        The downsampling scale factor between input image and output heatmap
+    mean : array-like of size 3
+        Mean pixel values to be subtracted from image tensor. Default is [0.485, 0.456, 0.406].
+    std : array-like of size 3
+        Standard deviation to be divided from image. Default is [0.229, 0.224, 0.225].
+    """
+    def __init__(self, width, height, num_class, scale_factor=4, mean=(0.485, 0.456, 0.406),
+                 std=(0.229, 0.224, 0.225), **kwargs):
+        self._width = width
+        self._height = height
+        self._num_class = num_class
+        self._scale_factor = scale_factor
+        self._mean = mean
+        self._std = std
+
+        from ....model_zoo.center_net.target_generator import CenterNetTargetGenerator
+        self._target_generator = CenterNetTargetGenerator(
+            num_class, width // scale_factor, height // scale_factor)
+
+    def __call__(self, src, label):
+        """Apply transform to training image/label."""
+        # random color jittering
+        img = experimental.image.random_color_distort(src)
+        # img = src
+        # bbox = label
+
+        # random expansion with prob 0.5
+        if np.random.uniform(0, 1) > 0.5:
+            img, expand = timage.random_expand(img, fill=[m * 255 for m in self._mean])
+            bbox = tbbox.translate(label, x_offset=expand[0], y_offset=expand[1])
+        else:
+            img, bbox = img, label
+
+        # random cropping
+        h, w, _ = img.shape
+        bbox, crop = experimental.bbox.random_crop_with_constraints(bbox, (w, h))
+        x0, y0, w, h = crop
+        img = mx.image.fixed_crop(img, x0, y0, w, h)
+
+        # random horizontal flip
+        h, w, _ = img.shape
+        img, flips = timage.random_flip(img, px=0.5)
+        bbox = tbbox.flip(bbox, (w, h), flip_x=flips[0])
+
+        # resize with random interpolation
+        h, w, _ = img.shape
+        interp = np.random.randint(0, 5)
+        img = timage.imresize(img, self._width, self._height, interp=interp)
+        bbox = tbbox.resize(bbox, (w, h), (self._width, self._height))
+        # cv2 = try_import_cv2()
+        # input_h, input_w = self._height, self._width
+        # s = max(h, w) * 1.0
+        # c = np.array([w / 2., h / 2.], dtype=np.float32)
+        # sf = 0.4
+        # cf = 0.1
+        # w_border = _get_border(128, img.shape[1])
+        # h_border = _get_border(128, img.shape[0])
+        # c[0] = np.random.randint(low=w_border, high=img.shape[1] - w_border)
+        # c[1] = np.random.randint(low=h_border, high=img.shape[0] - h_border)
+        # s = s * np.clip(np.random.randn()*sf + 1, 1 - sf, 1 + sf)
+        # trans_input = _get_affine_transform(c, s, 0, [input_w, input_h])
+        # inp = cv2.warpAffine(img.asnumpy(), trans_input, (input_w, input_h), flags=cv2.INTER_LINEAR)
+        # output_w = input_w // self._scale_factor
+        # output_h = input_h // self._scale_factor
+        # trans_output = _get_affine_transform(c, s, 0, [output_w, output_h])
+        # for i in range(bbox.shape[0]):
+        #     bbox[i, :2] = _affine_transform(bbox[i, :2], trans_output)
+        #     bbox[i, 2:4] = _affine_transform(bbox[i, 2:4], trans_output)
+        # bbox[:, :2] = np.clip(bbox[:, :2], 0, output_w - 1)
+        # bbox[:, 2:4] = np.clip(bbox[:, 2:4], 0, output_h - 1)
+        # img = mx.nd.array(inp)
+
+        # to tensor
+        img = mx.nd.image.to_tensor(img)
+        img = mx.nd.image.normalize(img, mean=self._mean, std=self._std)
+
+        # generate training target so cpu workers can help reduce the workload on gpu
+        gt_bboxes = bbox[:, :4]
+        gt_ids = bbox[:, 4:5]
+        ret = self._target_generator(img.shape[2], img.shape[1], gt_bboxes, gt_ids)
+        ret.update('input', img.asnumpy())
+        return ret
+
 
 class CenterNetDefaultValTransform(object):
     """Default SSD validation transform.
--- gluoncv/model_zoo/center_net/target_generator.py
+++ gluoncv/model_zoo/center_net/target_generator.py
@@ -42,6 +42,53 @@ class CenterNetTargetGenerator(gluon.Block):
                 center_reg_mask[:, center_y, center_x] = 1.0
         return tuple([nd.array(x) for x in (heatmap, wh_target, wh_mask, center_reg, center_reg_mask)])
 
+class CenterNetTargetGenerator(gluon.Block):
+    def __init__(self, num_class, output_width, output_height):
+        super(CenterNetTargetGenerator, self).__init__()
+        self._num_class = num_class
+        self._output_width = int(output_width)
+        self._output_height = int(output_height)
+
+    def forward(self, im_width, im_height, gt_boxes, gt_ids):
+        max_objs = 50
+        h_scale = float(self._output_height) / im_height
+        w_scale = float(self._output_width) / im_width
+        heatmap = np.zeros((self._num_class, self._output_height, self._output_width), dtype=np.float32)
+        # wh_target = np.zeros((2, self._output_height, self._output_width), dtype=np.float32)
+        # wh_mask = np.zeros((2, self._output_height, self._output_width), dtype=np.float32)
+        # center_reg = np.zeros((2, self._output_height, self._output_width), dtype=np.float32)
+        # center_reg_mask = np.zeros((2, self._output_height, self._output_width), dtype=np.float32)
+        wh_target = np.zeros((max_objs, 2), dtype=np.float32)
+        center_reg = np.zeros((max_objs, 2), dtype=np.float32)
+        ind = np.zeros((max_objs), dtype=np.int64)
+        reg_mask = np.zeros((max_objs), dtype=np.uint8)
+        k = 0
+        for bbox, cid in zip(gt_boxes, gt_ids):
+            cid = int(cid)
+            box_h, box_w = bbox[3] - bbox[1], bbox[2] - bbox[0]
+            if box_h > 0 and box_w > 0:
+                radius = _gaussian_radius((np.ceil(box_h), np.ceil(box_w)))
+                radius = max(0, int(radius))
+                center = np.array(
+                    [(bbox[0] + bbox[2]) / 2 * w_scale, (bbox[1] + bbox[3]) / 2 * h_scale], dtype=np.float32)
+                center_int = center.astype(np.int32)
+                center_x, center_y = center_int
+                assert center_x < self._output_width, 'center_x: {} > output_width: {}'.format(center_x, self._output_width)
+                assert center_y < self._output_height, 'center_y: {} > output_height: {}'.format(center_y, self._output_height)
+                _draw_umich_gaussian(heatmap[cid], center_int, radius)
+                # wh_target[0, center_y, center_x] = box_w * w_scale
+                # wh_target[1, center_y, center_x] = box_h * h_scale
+                # wh_mask[:, center_y, center_x] = 1.0
+                # center_reg[:, center_y, center_x] = center - center_int
+                # center_reg_mask[:, center_y, center_x] = 1.0
+                wh_target[k] = box_w, box_h
+                ind[k] = center_int[1] * output_w + center_int[0]
+                center_reg[k] = center - center_int
+                reg_mask[k] = 1
+            k += 1
+        ret = {'hm': heatmap, 'reg_mask': reg_mask, 'ind': ind, 'wh': wh_target, 'reg': center_reg}
+        return ret
+
 
 def _gaussian_radius(det_size, min_overlap=0.7):
   height, width = det_size
