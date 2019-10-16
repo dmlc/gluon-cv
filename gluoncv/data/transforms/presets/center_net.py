a1cfdc82bad67e8bf89f8b3f7cbc68a616816efd
Joshua Z. Zhang <cheungchih@gmail.com>
2019-10-16 13:11:42 -0700

    modify transform for train

--- gluoncv/data/transforms/presets/center_net.py
+++ gluoncv/data/transforms/presets/center_net.py
@@ -129,8 +129,16 @@ class CenterNetDefaultTrainTransform(object):
         self._height = height
         self._num_class = num_class
         self._scale_factor = scale_factor
-        self._mean = mean
-        self._std = std
+        self._mean = np.array(mean, dtype=np.float32).reshape(1, 1, 3)
+        self._std = np.array(std, dtype=np.float32).reshape(1, 1, 3)
+        self._data_rng = np.random.RandomState(123)
+        self._eig_val = np.array([0.2141788, 0.01817699, 0.00341571],
+                             dtype=np.float32)
+        self._eig_vec = np.array([
+            [-0.58752847, -0.69563484, 0.41340352],
+            [-0.5832747, 0.00994535, -0.81221408],
+            [-0.56089297, 0.71832671, 0.41158938]
+        ], dtype=np.float32)
 
         from ....model_zoo.center_net.target_generator import CenterNetTargetGenerator
         self._target_generator = CenterNetTargetGenerator(
@@ -139,59 +147,43 @@ class CenterNetDefaultTrainTransform(object):
     def __call__(self, src, label):
         """Apply transform to training image/label."""
         # random color jittering
-        img = experimental.image.random_color_distort(src)
-        # img = src
-        # bbox = label
-
-        # random expansion with prob 0.5
-        if np.random.uniform(0, 1) > 0.5:
-            img, expand = timage.random_expand(img, fill=[m * 255 for m in self._mean])
-            bbox = tbbox.translate(label, x_offset=expand[0], y_offset=expand[1])
-        else:
-            img, bbox = img, label
-
-        # random cropping
-        h, w, _ = img.shape
-        bbox, crop = experimental.bbox.random_crop_with_constraints(bbox, (w, h))
-        x0, y0, w, h = crop
-        img = mx.image.fixed_crop(img, x0, y0, w, h)
+        img = src
+        bbox = label
 
         # random horizontal flip
         h, w, _ = img.shape
         img, flips = timage.random_flip(img, px=0.5)
         bbox = tbbox.flip(bbox, (w, h), flip_x=flips[0])
 
-        # resize with random interpolation
-        h, w, _ = img.shape
-        interp = np.random.randint(0, 5)
-        img = timage.imresize(img, self._width, self._height, interp=interp)
-        bbox = tbbox.resize(bbox, (w, h), (self._width, self._height))
-        # cv2 = try_import_cv2()
-        # input_h, input_w = self._height, self._width
-        # s = max(h, w) * 1.0
-        # c = np.array([w / 2., h / 2.], dtype=np.float32)
-        # sf = 0.4
-        # cf = 0.1
-        # w_border = _get_border(128, img.shape[1])
-        # h_border = _get_border(128, img.shape[0])
-        # c[0] = np.random.randint(low=w_border, high=img.shape[1] - w_border)
-        # c[1] = np.random.randint(low=h_border, high=img.shape[0] - h_border)
-        # s = s * np.clip(np.random.randn()*sf + 1, 1 - sf, 1 + sf)
-        # trans_input = _get_affine_transform(c, s, 0, [input_w, input_h])
-        # inp = cv2.warpAffine(img.asnumpy(), trans_input, (input_w, input_h), flags=cv2.INTER_LINEAR)
-        # output_w = input_w // self._scale_factor
-        # output_h = input_h // self._scale_factor
-        # trans_output = _get_affine_transform(c, s, 0, [output_w, output_h])
-        # for i in range(bbox.shape[0]):
-        #     bbox[i, :2] = _affine_transform(bbox[i, :2], trans_output)
-        #     bbox[i, 2:4] = _affine_transform(bbox[i, 2:4], trans_output)
-        # bbox[:, :2] = np.clip(bbox[:, :2], 0, output_w - 1)
-        # bbox[:, 2:4] = np.clip(bbox[:, 2:4], 0, output_h - 1)
-        # img = mx.nd.array(inp)
+        cv2 = try_import_cv2()
+        input_h, input_w = self._height, self._width
+        s = max(h, w) * 1.0
+        c = np.array([w / 2., h / 2.], dtype=np.float32)
+        sf = 0.4
+        cf = 0.1
+        w_border = _get_border(128, img.shape[1])
+        h_border = _get_border(128, img.shape[0])
+        c[0] = np.random.randint(low=w_border, high=img.shape[1] - w_border)
+        c[1] = np.random.randint(low=h_border, high=img.shape[0] - h_border)
+        s = s * np.clip(np.random.randn()*sf + 1, 1 - sf, 1 + sf)
+        trans_input = _get_affine_transform(c, s, 0, [input_w, input_h])
+        inp = cv2.warpAffine(img.asnumpy(), trans_input, (input_w, input_h), flags=cv2.INTER_LINEAR)
+        output_w = input_w // self._scale_factor
+        output_h = input_h // self._scale_factor
+        trans_output = _get_affine_transform(c, s, 0, [output_w, output_h])
+        for i in range(bbox.shape[0]):
+            bbox[i, :2] = _affine_transform(bbox[i, :2], trans_output)
+            bbox[i, 2:4] = _affine_transform(bbox[i, 2:4], trans_output)
+        bbox[:, :2] = np.clip(bbox[:, :2], 0, output_w - 1)
+        bbox[:, 2:4] = np.clip(bbox[:, 2:4], 0, output_h - 1)
+        img = inp
 
         # to tensor
-        img = mx.nd.image.to_tensor(img)
-        img = mx.nd.image.normalize(img, mean=self._mean, std=self._std)
+        img = img.astype(np.float32) / 255.
+        color_aug(self._data_rng, img, self._eig_val, self._eig_vec)
+        img = (img - self._mean) / self._std
+        img = img.transpose(2, 0, 1).astype(np.float32)
+        img = mx.nd.array(img)
 
         # generate training target so cpu workers can help reduce the workload on gpu
         gt_bboxes = bbox[:, :4]
