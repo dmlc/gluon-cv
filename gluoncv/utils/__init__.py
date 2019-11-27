705ab3a9825b2f287e4d52b23b324dbcf9dc75cf
Joshua Z. Zhang <cheungchih@gmail.com>
2019-11-27 10:38:12 -0800

    address comments

--- gluoncv/utils/__init__.py
+++ gluoncv/utils/__init__.py
@@ -8,9 +8,7 @@ from . import metrics
 from . import parallel
 
 from .download import download, check_sha1
-from .filesystem import makedirs
-from .filesystem import try_import_dali
-from .filesystem import try_import_cv2
+from .filesystem import makedirs, try_import_dali, try_import_cv2
 from .bbox import bbox_iou
 from .block import recursive_visit, set_lr_mult, freeze_bn
 from .lr_scheduler import LRSequential, LRScheduler
