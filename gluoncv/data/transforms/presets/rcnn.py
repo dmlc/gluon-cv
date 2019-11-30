55ad2db256d47f1f433d0b7d0be6a6a7783ea6f4
bin/choo <079111w@gmail.com>
2019-12-01 03:53:26 +0900

    Update rcnn.py

--- gluoncv/data/transforms/presets/rcnn.py
+++ gluoncv/data/transforms/presets/rcnn.py
@@ -50,7 +50,7 @@ def transform_test(imgs, short=600, max_size=1000, mean=(0.485, 0.456, 0.406),
         if dim_imgs == 3:
             imgs = [imgs]
         elif dim_imgs == 4:
-            imgs = [*imgs]
+            imgs = [img for img in imgs]
 
     for im in imgs:
         assert isinstance(im, mx.nd.NDArray), "Expect NDArray, got {}".format(type(im))
