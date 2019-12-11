bca14b75c0e8b6b1cb74447499770e0a337c1f0c
Serge Panev <spanev@nvidia.com>
2019-12-11 22:41:04 +0100

    Scale lr and lr_warmup with batch size in M-RCNN training (#988)
    * Scale lr and lr_warmup with batch size in M-RCNN training
    
    Signed-off-by: Serge Panev <spanev@nvidia.com>
    
    * Limit lr_warmup to 1000
    
    Signed-off-by: Serge Panev <spanev@nvidia.com>

--- scripts/instance/mask_rcnn/train_mask_rcnn.py
+++ scripts/instance/mask_rcnn/train_mask_rcnn.py
@@ -134,8 +134,8 @@ def parse_args():
         hvd.init()
     args.epochs = int(args.epochs) if args.epochs else 26
     args.lr_decay_epoch = args.lr_decay_epoch if args.lr_decay_epoch else '17,23'
-    args.lr = float(args.lr) if args.lr else 0.01
-    args.lr_warmup = args.lr_warmup if args.lr_warmup else 1000
+    args.lr = float(args.lr) if args.lr else (0.00125 * args.batch_size)
+    args.lr_warmup = args.lr_warmup if args.lr_warmup else max((8000 / args.batch_size), 1000)
     args.wd = float(args.wd) if args.wd else 1e-4
     return args
 
