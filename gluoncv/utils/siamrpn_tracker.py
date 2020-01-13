405f8150e48e7c98b7cafbad15816ef8ffdce498
Ubuntu <chenyukunin1906@163.com>
2020-01-13 03:58:06 +0000

    change gif and fix demo

--- gluoncv/utils/siamrpn_tracker.py
+++ gluoncv/utils/siamrpn_tracker.py
@@ -372,28 +372,3 @@ class SiamRPNTracker(BaseTracker):
         return {'bbox': bbox,
                 'best_score': best_score}
 
-def get_axis_aligned_bbox(region):
-    """ convert region to (cx, cy, w, h) that represent by axis aligned box
-    """
-    nv = region.size
-    if nv == 8:
-        cx = np.mean(region[0::2])
-        cy = np.mean(region[1::2])
-        x1 = min(region[0::2])
-        x2 = max(region[0::2])
-        y1 = min(region[1::2])
-        y2 = max(region[1::2])
-        A1 = np.linalg.norm(region[0:2] - region[2:4]) * \
-            np.linalg.norm(region[2:4] - region[4:6])
-        A2 = (x2 - x1) * (y2 - y1)
-        s = np.sqrt(A1 / A2)
-        w = s * (x2 - x1) + 1
-        h = s * (y2 - y1) + 1
-    else:
-        x = region[0]
-        y = region[1]
-        w = region[2]
-        h = region[3]
-        cx = x+w/2
-        cy = y+h/2
-    return cx, cy, w, h
