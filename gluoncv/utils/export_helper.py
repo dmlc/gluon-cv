6c57ae92a23cf1d0680f7c24fdd610da6b1016d5
Joshua Z. Zhang <cheungchih@gmail.com>
2020-02-26 11:20:25 -0800

    fix frcnn export

--- gluoncv/utils/export_helper.py
+++ gluoncv/utils/export_helper.py
@@ -1,5 +1,6 @@
 """Helper utils for export HybridBlock to symbols."""
 from __future__ import absolute_import
+import copy
 import mxnet as mx
 from mxnet.base import MXNetError
 from mxnet.gluon import HybridBlock
@@ -87,6 +88,11 @@ def export_block(path, block, data_shape=None, epoch=0, preprocess=True, layout=
     else:
         data_shapes = [data_shape]
 
+    # use deepcopy of network to avoid in-place modification
+    copy_block = copy.deepcopy(block)
+    if '_target_generator' in copy_block._children:
+        copy_block._children.pop('_target_generator')
+
     if preprocess:
         # add preprocess block
         if preprocess is True:
@@ -99,9 +105,9 @@ def export_block(path, block, data_shape=None, epoch=0, preprocess=True, layout=
         wrapper_block = nn.HybridSequential()
         preprocess.initialize(ctx=ctx)
         wrapper_block.add(preprocess)
-        wrapper_block.add(block)
+        wrapper_block.add(copy_block)
     else:
-        wrapper_block = block
+        wrapper_block = copy_block
         assert layout in ('CHW', 'CTHW'), \
             "Default layout is CHW for 2D models and CTHW for 3D models if preprocess is None," \
             + " provided {}.".format(layout)
