a004e0b9f295189923c84783c4096cde64ccb9a6
bryanzhu <yizhu59@gmail.com>
2020-03-08 14:07:17 -0700

    rm 2 test cases

--- tests/unittests/test_model_zoo.py
+++ tests/unittests/test_model_zoo.py
@@ -678,15 +678,6 @@ def test_quantized_action_recognition_models():
     x = mx.random.uniform(shape=(1 * num_segments, 3, 224, 224), ctx=ctx)
     _test_model_list(model_list, ctx, x)
 
-    model_list = ['inceptionv3_ucf101_int8', 'inceptionv3_kinetics400_int8']
-    x = mx.random.uniform(shape=(1 * num_segments, 3, 299, 299), ctx=ctx)
-    _test_model_list(model_list, ctx, x)
-
-    num_segments = 7
-    model_list = ['resnet18_v1b_kinetics400_int8']
-    x = mx.random.uniform(shape=(1 * num_segments, 3, 224, 224), ctx=ctx)
-    _test_model_list(model_list, ctx, x)
-
 
 if __name__ == '__main__':
     import nose
