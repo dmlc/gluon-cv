ffe8b4a5f2b3de434d8326ad8cc90b6569dc274a
xdeng7 <xdeng7@ucmerced.edu>
2020-04-28 21:58:48 -0700

    danet.py

--- gluoncv/model_zoo/danet.py
+++ gluoncv/model_zoo/danet.py
@@ -2,12 +2,10 @@
 from mxnet.gluon import nn
 from mxnet.context import cpu
 from mxnet.gluon.nn import HybridBlock
-from mxnet import gluon
-from .resnet import get_resnet
 from .segbase import SegBaseModel
 from .fcn import _FCNHead
 from .attention import PAM_Module, CAM_Module
-# pylint: disable-all
+
 
 __all__ = ['DANet', 'get_danet', 'get_danet_resnet50_citys', 'get_danet_resnet101_citys']
 
@@ -28,10 +26,10 @@ class DANet(SegBaseModel):
     """
 
     def __init__(self, nclass, backbone='resnet50', aux=False, ctx=cpu(), pretrained_base=True,
-                height=None, width=None,base_size=576, crop_size=512, dilated=True, **kwargs):
+                 height=None, width=None, base_size=576, crop_size=512, dilated=True, **kwargs):
         super(DANet, self).__init__(nclass, aux, backbone, ctx=ctx, base_size=base_size,
-                crop_size=crop_size, pretrained_base=pretrained_base, **kwargs)
-        self.aux=aux
+                                    crop_size=crop_size, pretrained_base=pretrained_base, **kwargs)
+        self.aux = aux
         height = height if height is not None else crop_size
         width = width if width is not None else crop_size
 
@@ -68,18 +66,18 @@ class DANet(SegBaseModel):
         return tuple(outputs)
         
 class DANetHead(HybridBlock):
-    def __init__(self, in_channels, out_channels, norm_layer=nn.BatchNorm, norm_kwargs=None,**kwargs):
+    def __init__(self, in_channels, out_channels, norm_layer=nn.BatchNorm, norm_kwargs=None, **kwargs):
         super(DANetHead, self).__init__()
         inter_channels = in_channels // 4
         self.conv5a = nn.HybridSequential()
         self.conv5a.add(nn.Conv2D(in_channels=in_channels, channels=inter_channels, kernel_size=3, 
-                                                            padding=1, use_bias=False))
+                                  padding=1, use_bias=False))
         self.conv5a.add(norm_layer(in_channels=inter_channels, **({} if norm_kwargs is None else norm_kwargs)))
         self.conv5a.add(nn.Activation('relu'))
 
         self.conv5c = nn.HybridSequential()
         self.conv5c.add(nn.Conv2D(in_channels=in_channels, channels=inter_channels, kernel_size=3, 
-                                                            padding=1, use_bias=False))
+                                  padding=1, use_bias=False))
         self.conv5c.add(norm_layer(in_channels=inter_channels, **({} if norm_kwargs is None else norm_kwargs)))
         self.conv5c.add(nn.Activation('relu'))
         
@@ -87,27 +85,27 @@ class DANetHead(HybridBlock):
         self.sc = CAM_Module(inter_channels)
         self.conv51 = nn.HybridSequential()
         self.conv51.add(nn.Conv2D(in_channels=inter_channels, channels=inter_channels, kernel_size=3, 
-                                                            padding=1, use_bias=False))
+                                  padding=1, use_bias=False))
         self.conv51.add(norm_layer(in_channels=inter_channels, **({} if norm_kwargs is None else norm_kwargs)))
         self.conv51.add(nn.Activation('relu'))
 
         self.conv52 = nn.HybridSequential()
         self.conv52.add(nn.Conv2D(in_channels=inter_channels, channels=inter_channels, kernel_size=3, 
-                                                            padding=1, use_bias=False))
+                                  padding=1, use_bias=False))
         self.conv52.add(norm_layer(in_channels=inter_channels, **({} if norm_kwargs is None else norm_kwargs)))
         self.conv52.add(nn.Activation('relu'))
 
-        self.conv6=nn.HybridSequential()
-        self.conv6.add( nn.Conv2D(in_channels=512, channels=out_channels, kernel_size=1))
-        self.conv6.add( nn.Dropout(0.1))
+        self.conv6 = nn.HybridSequential()
+        self.conv6.add(nn.Conv2D(in_channels=512, channels=out_channels, kernel_size=1))
+        self.conv6.add(nn.Dropout(0.1))
 
-        self.conv7=nn.HybridSequential()
-        self.conv7.add( nn.Conv2D(in_channels=512, channels=out_channels, kernel_size=1))
-        self.conv7.add( nn.Dropout(0.1))
+        self.conv7 = nn.HybridSequential()
+        self.conv7.add(nn.Conv2D(in_channels=512, channels=out_channels, kernel_size=1))
+        self.conv7.add(nn.Dropout(0.1))
 
-        self.conv8=nn.HybridSequential()
-        self.conv8.add( nn.Conv2D(in_channels=512, channels=out_channels, kernel_size=1))
-        self.conv8.add( nn.Dropout(0.1))
+        self.conv8 = nn.HybridSequential()
+        self.conv8.add(nn.Conv2D(in_channels=512, channels=out_channels, kernel_size=1))
+        self.conv8.add(nn.Dropout(0.1))
 
 
     def hybrid_forward(self, F, x):
@@ -122,8 +120,7 @@ class DANetHead(HybridBlock):
         sc_conv = self.conv52(sc_feat)
         sc_output = self.conv7(sc_conv)
 
-        feat_sum = sa_conv+sc_conv
-        
+        feat_sum = sa_conv+sc_conv 
         sasc_output = self.conv8(feat_sum)
 
         output = [sasc_output]
@@ -136,7 +133,7 @@ class DANetHead(HybridBlock):
         h, w = x.shape[2:]
         self._up_kwargs['height'] = h
         self._up_kwargs['width'] = w
-        c3, c4 = self.base_forward(x)
+        _, c4 = self.base_forward(x)
         x = self.head.demo(c4)
         import mxnet.ndarray as F
         pred = F.contrib.BilinearResize2D(x, **self._up_kwargs)
@@ -144,7 +141,7 @@ class DANetHead(HybridBlock):
 
 
 def get_danet(dataset='pascal_voc', backbone='resnet50', pretrained=False,
-            root='~/.mxnet/models', ctx=cpu(0), **kwargs):
+              root='~/.mxnet/models', ctx=cpu(0), **kwargs):
     r"""DANet model from the paper `"Dual Attention Network for Scene Segmentation"
     <https://arxiv.org/abs/1809.02983>`
     """
@@ -155,7 +152,6 @@ def get_danet(dataset='pascal_voc', backbone='resnet50', pretrained=False,
         'coco': 'coco',
         'citys': 'citys',
     }
-    
     from ..data import datasets
     # infer number of classes
     model = DANet(nclass=datasets[dataset].NUM_CLASS, backbone=backbone, ctx=ctx, **kwargs)
@@ -203,4 +199,3 @@ def get_danet_resnet101_citys(**kwargs):
     >>> print(model)
     """
     return get_danet('citys', 'resnet101', **kwargs)
-
