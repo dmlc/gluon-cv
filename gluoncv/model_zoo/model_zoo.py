06b19e8d4dc54bd162fd567858c0bef1a7171e38
xdeng7 <xdeng7@ucmerced.edu>
2020-04-28 21:32:07 -0700

    attention.py

--- gluoncv/model_zoo/model_zoo.py
+++ gluoncv/model_zoo/model_zoo.py
@@ -40,6 +40,7 @@ from .center_net import *
 from .hrnet import *
 from .siamrpn import *
 from .icnet import *
+from .danet import *
 
 __all__ = ['get_model', 'get_model_list']
 
@@ -184,6 +185,8 @@ _models = {
     'deeplab_v3b_plus_wideresnet_citys': get_deeplab_v3b_plus_wideresnet_citys,
     'icnet_resnet50_citys': get_icnet_resnet50_citys,
     'icnet_resnet50_mhpv1': get_icnet_resnet50_mhpv1,
+    'danet_resnet50_citys':get_danet_resnet50_citys,
+    'danet_resnet101_citys':get_danet_resnet101_citys,
     'resnet18_v1b': resnet18_v1b,
     'resnet34_v1b': resnet34_v1b,
     'resnet50_v1b': resnet50_v1b,
