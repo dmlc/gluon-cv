95b482d7be279c5b541cbde18e39308dc42608ba
Joshua Z. Zhang <cheungchih@gmail.com>
2020-05-21 12:12:13 -0700

    pin version

--- gluoncv/__init__.py
+++ gluoncv/__init__.py
@@ -7,14 +7,7 @@ __version__ = '0.8.0'
 
 from .utils.version import _require_mxnet_version, _deprecate_python2
 _deprecate_python2()
-_require_mxnet_version('1.4.0')
-try:
-    # pylint: disable=unused-import
-    from mxnet import metric
-except ImportError:
-    from mxnet.gluon import metric
-    import mxnet
-    mxnet.metric = metric
+_require_mxnet_version('1.4.0', '2.0.0')
 
 from . import data
 from . import model_zoo
