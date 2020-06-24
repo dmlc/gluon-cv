94b9868c41fc988cc2b57c29103ce43f9f3dd7cc
xinyu <xinyu1.chen@intel.com>
2020-06-24 11:17:24 +0800

    add docstring

--- gluoncv/model_zoo/ssd/resnet_v1b_ssd.py
+++ gluoncv/model_zoo/ssd/resnet_v1b_ssd.py
@@ -8,14 +8,38 @@ from ..model_zoo import get_model
 
 
 class ResNetV1bSSD(HybridBlock):
-    def __init__(self, name, add_filters,
+    """Single-shot Object Detection Network: https://arxiv.org/abs/1512.02325.
+       with resnetv1b base model.
+
+    Parameters
+    ----------
+    network : string or None
+        Name of the base network, must be end with v1b.
+    add_filters : list of int
+        Number of channels for the appended layers, ignored if `network`is `None`.
+    norm_layer : object
+        Normalization layer used (default: :class:`mxnet.gluon.nn.BatchNorm`)
+        Can be :class:`mxnet.gluon.nn.BatchNorm` or :class:`mxnet.gluon.contrib.nn.SyncBatchNorm`.
+        This will only apply to base networks that has `norm_layer` specified, will ignore if the
+        base network (e.g. VGG) don't accept this argument.
+    norm_kwargs : dict
+        Additional `norm_layer` arguments, for example `num_devices=4`
+        for :class:`mxnet.gluon.contrib.nn.SyncBatchNorm`.
+    use_bn : bool
+        Whether to use BatchNorm layer after each attached convolutional layer.
+    reduce_ratio : float
+        Channel reduce ratio (0, 1) of the transition layer.
+    min_depth : int
+        Minimum channels for the transition layers.
+    """
+    def __init__(self, network, add_filters,
                  norm_layer=BatchNorm, norm_kwargs=None,
-                 batch_norm=False, reduce_ratio=1.0, min_depth=128, **kwargs):
+                 use_bn=False, reduce_ratio=1.0, min_depth=128, **kwargs):
         super(ResNetV1bSSD, self).__init__()
-        assert name.endswith('v1b')
+        assert network.endswith('v1b')
         if norm_kwargs is None:
             norm_kwargs = {}
-        res = get_model(name, **kwargs)
+        res = get_model(network, **kwargs)
         weight_init = mx.init.Xavier(rnd_type='gaussian', factor_type='out', magnitude=2)
         with self.name_scope():
             self.stage1 = HybridSequential('stage1')
@@ -32,16 +56,16 @@ class ResNetV1bSSD(HybridBlock):
             for i, num_filter in enumerate(add_filters):
                 stage = HybridSequential('more_stages_' + str(i))
                 num_trans = max(min_depth, int(round(num_filter * reduce_ratio)))
-                stage.add(Conv2D(channels=num_trans, kernel_size=1, use_bias=not batch_norm,
+                stage.add(Conv2D(channels=num_trans, kernel_size=1, use_bias=not use_bn,
                                  weight_initializer=weight_init))
-                if batch_norm:
+                if use_bn:
                     stage.add(norm_layer(**norm_kwargs))
                 stage.add(Activation('relu'))
                 padding = 0 if i == len(add_filters) - 1 else 1
                 stage.add(Conv2D(channels=num_filter, kernel_size=3,
-                                 strides=2, padding=padding, use_bias=not batch_norm,
+                                 strides=2, padding=padding, use_bias=not use_bn,
                                  weight_initializer=weight_init))
-                if batch_norm:
+                if use_bn:
                     stage.add(norm_layer(**norm_kwargs))
                 stage.add(Activation('relu'))
                 self.more_stages.add(stage)
@@ -57,4 +81,4 @@ class ResNetV1bSSD(HybridBlock):
         return more_out
 
 def resnet34_v1b_ssd(**kwargs):
-    return ResNetV1bSSD(name='resnet34_v1b', add_filters=[256, 256, 128, 128], **kwargs)
+    return ResNetV1bSSD(network='resnet34_v1b', add_filters=[256, 256, 128, 128], **kwargs)
