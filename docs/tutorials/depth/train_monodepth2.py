9df6532ed99871c0b300ec51b741509e2ac17cd2
Kuang <haofeik@f8ffc22b1e09.ant.amazon.com>
2020-07-29 22:39:15 +0800

    update pr

--- docs/tutorials/depth/train_monodepth2.py
+++ docs/tutorials/depth/train_monodepth2.py
@@ -101,64 +101,64 @@ import gluoncv
 # The Decoder module is a fully convolutional network with skip architecture, it expolit the featuremaps
 # in different scale and concatenating together after upsampling. It is defined as:
 #
-# class DepthDecoder(nn.HybridBlock):
-#     def __init__(self, num_ch_enc, scales=range(4), num_output_channels=1,
-#                  use_skips=True):
-#         super(DepthDecoder, self).__init__()
+#     class DepthDecoder(nn.HybridBlock):
+#         def __init__(self, num_ch_enc, scales=range(4), num_output_channels=1,
+#                      use_skips=True):
+#             super(DepthDecoder, self).__init__()
 #
-#         self.num_output_channels = num_output_channels
-#         self.use_skips = use_skips
-#         self.upsample_mode = 'nearest'
-#         self.scales = scales
+#             self.num_output_channels = num_output_channels
+#             self.use_skips = use_skips
+#             self.upsample_mode = 'nearest'
+#             self.scales = scales
 #
-#         self.num_ch_enc = num_ch_enc
-#         self.num_ch_dec = np.array([16, 32, 64, 128, 256])
+#             self.num_ch_enc = num_ch_enc
+#             self.num_ch_dec = np.array([16, 32, 64, 128, 256])
 #
-#         # decoder
-#         with self.name_scope():
-#             self.convs = OrderedDict()
+#             # decoder
+#             with self.name_scope():
+#                 self.convs = OrderedDict()
+#                 for i in range(4, -1, -1):
+#                     # upconv_0
+#                     num_ch_in = self.num_ch_enc[-1] if i == 4 else self.num_ch_dec[i + 1]
+#                     num_ch_out = self.num_ch_dec[i]
+#                     self.convs[("upconv", i, 0)] = ConvBlock(num_ch_in, num_ch_out)
+#
+#                     # upconv_1
+#                     num_ch_in = self.num_ch_dec[i]
+#                     if self.use_skips and i > 0:
+#                         num_ch_in += self.num_ch_enc[i - 1]
+#                     num_ch_out = self.num_ch_dec[i]
+#                     self.convs[("upconv", i, 1)] = ConvBlock(num_ch_in, num_ch_out)
+#
+#                 for s in self.scales:
+#                     self.convs[("dispconv", s)] = Conv3x3(
+#                         self.num_ch_dec[s], self.num_output_channels)
+#
+#                 # register blocks
+#                 for k in self.convs:
+#                     self.register_child(self.convs[k])
+#                 self.decoder = nn.HybridSequential()
+#                 self.decoder.add(*list(self.convs.values()))
+#
+#                 self.sigmoid = nn.Activation('sigmoid')
+#
+#         def hybrid_forward(self, F, input_features):
+#             # pylint: disable=unused-argument, missing-function-docstring
+#             self.outputs = []
+#
+#             # decoder
+#             x = input_features[-1]
 #             for i in range(4, -1, -1):
-#                 # upconv_0
-#                 num_ch_in = self.num_ch_enc[-1] if i == 4 else self.num_ch_dec[i + 1]
-#                 num_ch_out = self.num_ch_dec[i]
-#                 self.convs[("upconv", i, 0)] = ConvBlock(num_ch_in, num_ch_out)
-#
-#                 # upconv_1
-#                 num_ch_in = self.num_ch_dec[i]
+#                 x = self.convs[("upconv", i, 0)](x)
+#                 x = [F.UpSampling(x, scale=2, sample_type='nearest')]
 #                 if self.use_skips and i > 0:
-#                     num_ch_in += self.num_ch_enc[i - 1]
-#                 num_ch_out = self.num_ch_dec[i]
-#                 self.convs[("upconv", i, 1)] = ConvBlock(num_ch_in, num_ch_out)
+#                     x += [input_features[i - 1]]
+#                 x = F.concat(*x, dim=1)
+#                 x = self.convs[("upconv", i, 1)](x)
+#                 if i in self.scales:
+#                     self.outputs.append(self.sigmoid(self.convs[("dispconv", i)](x)))
 #
-#             for s in self.scales:
-#                 self.convs[("dispconv", s)] = Conv3x3(
-#                     self.num_ch_dec[s], self.num_output_channels)
-#
-#             # register blocks
-#             for k in self.convs:
-#                 self.register_child(self.convs[k])
-#             self.decoder = nn.HybridSequential()
-#             self.decoder.add(*list(self.convs.values()))
-#
-#             self.sigmoid = nn.Activation('sigmoid')
-#
-#     def hybrid_forward(self, F, input_features):
-#         # pylint: disable=unused-argument, missing-function-docstring
-#         self.outputs = []
-#
-#         # decoder
-#         x = input_features[-1]
-#         for i in range(4, -1, -1):
-#             x = self.convs[("upconv", i, 0)](x)
-#             x = [F.UpSampling(x, scale=2, sample_type='nearest')]
-#             if self.use_skips and i > 0:
-#                 x += [input_features[i - 1]]
-#             x = F.concat(*x, dim=1)
-#             x = self.convs[("upconv", i, 1)](x)
-#             if i in self.scales:
-#                 self.outputs.append(self.sigmoid(self.convs[("dispconv", i)](x)))
-#
-#         return self.outputs
+#             return self.outputs
 #
 # Monodepth model is provided in :class:`gluoncv.model_zoo.MonoDepth2`. To get
 # Monodepth2 model using ResNet18 base network:
