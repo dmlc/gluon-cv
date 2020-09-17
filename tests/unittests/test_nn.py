5d541139ddbbbac6c125b589c8ffb8aa97a2a04d
Jinbo Ci <32102965+jinboci@users.noreply.github.com>
2020-09-17 04:17:43 +0000

    Update test_nn.py

--- tests/unittests/test_nn.py
+++ tests/unittests/test_nn.py
@@ -9,7 +9,7 @@ def test_groupnorm():
     gn.initialize(ctx=ctx)
     y = gn(x)
     y = y.reshape(y.shape[0], 4, -1)
-    print('y.mean(2) = ', y.mean(2))
+    print('y.mean(2) =', y.mean(2))
     mx.test_utils.assert_almost_equal(y.mean(2).asnumpy(),
                                       mx.np.zeros_like(y.mean(2)).asnumpy(),
                                       rtol=1e-3, atol=1e-3)
