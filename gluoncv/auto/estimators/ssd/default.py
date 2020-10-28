0cbbf841c780f44ecd6da16d9879286ea117aa34
Joshua Z. Zhang <cheungchih@gmail.com>
2020-10-27 17:38:42 -0700

    add types

--- gluoncv/auto/estimators/ssd/default.py
+++ gluoncv/auto/estimators/ssd/default.py
@@ -7,65 +7,65 @@ from typing import Union, Tuple
 @dataclass
 class SSD:
     # Backbone network.
-    backbone = 'vgg16_atrous'  # base feature network
+    backbone : str = 'vgg16_atrous'  # base feature network
     # Input data shape, use 300, 512.
-    data_shape = 300
+    data_shape : int = 300
     # List of convolution layer channels which is going to be appended to the base
     # network feature extractor. If `name` is `None`, this is ignored.
-    filters = None
+    filters : Union[int, None] = None
     # Sizes of anchor boxes, this should be a list of floats, in incremental order.
     # The length of `sizes` must be len(layers) + 1.
-    sizes = [30, 60, 111, 162, 213, 264, 315]
+    sizes : Union[Tuple, list] = [30, 60, 111, 162, 213, 264, 315]
     # Aspect ratios of anchors in each output layer. Its length must be equals
     # to the number of SSD output layers.
-    ratios = [[1, 2, 0.5]] + [[1, 2, 0.5, 3, 1.0/3]] * 3 + [[1, 2, 0.5]] * 2
+    ratios : Union[Tuple, list] = [[1, 2, 0.5]] + [[1, 2, 0.5, 3, 1.0/3]] * 3 + [[1, 2, 0.5]] * 2
     # Step size of anchor boxes in each output layer.
-    steps = [8, 16, 32, 64, 100, 300]
+    steps : Union[Tuple, list] = [8, 16, 32, 64, 100, 300]
     # Use synchronize BN across devices.
-    syncbn = False
+    syncbn : bool = False
     # Whether to use automatic mixed precision
-    amp = False
+    amp : bool = False
     # Whether to enable custom model.
     # custom_model = True
     # whether apply transfer learning from pre-trained models, if True, override other net structures
-    transfer = 'ssd_512_resnet50_v1_coco'
+    transfer : Union[str, None] = 'ssd_512_resnet50_v1_coco'
 
 
 @dataclass
 class TrainCfg:
     # Batch size during training
-    batch_size = 32
+    batch_size : int = 32
     # starting epoch
-    start_epoch = 0
+    start_epoch : int = 0
     # total epoch for training
-    epochs = 20
+    epochs : int = 20
     # Learning rate.
-    lr = 0.001
+    lr : float = 0.001
     # Decay rate of learning rate.
-    lr_decay = 0.1
+    lr_decay : float = 0.1
     # Epochs at which learning rate decays
-    lr_decay_epoch = (160, 200)
+    lr_decay_epoch : Union[Tuple, list] = (160, 200)
     # Momentum
-    momentum = 0.9
+    momentum : float = 0.9
     # Weight decay
-    wd = 5e-4
+    wd : float = 5e-4
     # log interval in terms of iterations
-    log_interval = 100
+    log_interval : int = 100
     # Random seed to be fixed.
-    seed = 233
+    seed : int = 233
     # Use DALI for data loading and data preprocessing in training.
     # Currently supports only COCO.
-    dali = False
+    dali : bool = False
 
 
 @dataclass
 class ValidCfg:
     # Epoch interval for validation
-    val_interval = 1
+    val_interval : int = 1
     # metric, 'voc', 'voc07'
-    metric = 'voc07'
+    metric : str = 'voc07'
     # iou_thresh for VOC type metrics
-    iou_thresh = 0.5
+    iou_thresh : float = 0.5
 
 @dataclass
 class SSDCfg:
@@ -73,20 +73,18 @@ class SSDCfg:
     train : TrainCfg = field(default_factory=TrainCfg)
     valid : ValidCfg = field(default_factory=ValidCfg)
     # Dataset name. eg. 'coco', 'voc', 'voc_tiny'
-    dataset = 'voc_tiny'
+    dataset : str = 'voc_tiny'
     # Path of the directory where the dataset is located.
-    dataset_root = '~/.mxnet/datasets/'
+    dataset_root : str = '~/.mxnet/datasets/'
     # Training with GPUs, you can specify (1,3) for example.
-    gpus = (0, 1, 2, 3)
+    gpus : Union[Tuple, list] = (0, 1, 2, 3)
     # Resume from previously saved parameters if not None.
     # For example, you can resume from ./faster_rcnn_xxx_0123.params.
-    resume = ''
-    # Saving parameter prefix
-    save_prefix = ''
+    resume : str = ''
     # Saving parameters epoch interval, best model will always be saved.
-    save_interval = 1
+    save_interval : int = 1
     # Use MXNet Horovod for distributed training. Must be run with OpenMPI.
-    horovod = False
+    horovod : bool = False
     # Number of data workers, you can use larger number to accelerate data loading,
     # if your CPU and GPUs are powerful.
-    num_workers = 4
+    num_workers : int = 4
