634c3be0dde9516ee091daaafb14e281ae98c681
Joshua Z. Zhang <cheungchih@gmail.com>
2020-10-27 17:12:08 -0700

    fix import

--- gluoncv/auto/tasks/__init__.py
+++ gluoncv/auto/tasks/__init__.py
@@ -1,4 +1,4 @@
 """AutoML Tasks"""
-# from .image_classification import *
-# from .object_detection import *
+from .image_classification import *
+from .object_detection import *
 # from .utils import *
