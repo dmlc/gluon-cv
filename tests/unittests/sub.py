f201f99aa6b87967f213bc3a691a355cf4b9af84
yinweisu <wyin@ucdavis.edu>
2020-11-04 16:15:30 -0800

    fix

--- tests/unittests/sub.py
+++ tests/unittests/sub.py
@@ -6,7 +6,7 @@ from shutil import copyfile
 parser = argparse.ArgumentParser()
 parser.add_argument('--data', type=str, required=True, help='path to the coco image data')
 parser.add_argument('--annotation', type=str, required=True, help='path to the annotation')
-parser.add_argument('--dest', type=str, default='.', help='path of the annotation file')
+parser.add_argument('--dest', type=str, default='.', help='path to store the result')
 args = parser.parse_args()
 
 data_path = os.path.expanduser(args.data)
