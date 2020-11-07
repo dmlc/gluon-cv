e6d4e199d254e25f3145de7c601cdb08e7000bc9
jinboci <ci.jinbo@outlook.com>
2020-11-07 10:34:58 +0000

    remove unrelated files

--- gluoncv/model_zoo/resnext.py
+++ gluoncv/model_zoo/resnext.py
@@ -107,20 +107,19 @@ class Block(HybridBlock):
             self.downsample = None
 
     def hybrid_forward(self, F, x):
-        x = x.as_np_ndarray()
         residual = x
 
         x = self.body(x)
 
         if self.se:
-            w = F.contrib.AdaptiveAvgPooling2D(x.as_nd_ndarray(), output_size=1).as_np_ndarray()
+            w = F.contrib.AdaptiveAvgPooling2D(x, output_size=1)
             w = self.se(w)
-            x = x * w
+            x = F.broadcast_mul(x, w)
 
         if self.downsample:
             residual = self.downsample(residual)
 
-        x = F.npx.activation(x + residual, act_type='relu')
+        x = F.Activation(x + residual, act_type='relu')
         return x
 
 
