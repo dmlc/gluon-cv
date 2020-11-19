7d43e282d4944c179406c22a164d1b62ac4fcd5c
bryanzhu <yizhu59@gmail.com>
2020-11-19 15:06:36 -0800

    format

--- docs/tutorials_torch/action_recognition/finetune_custom.py
+++ docs/tutorials_torch/action_recognition/finetune_custom.py
@@ -9,6 +9,8 @@ we will take care of the rest.
 You can start fine-tuning from many popular pre-trained models (e.g., I3D, R2+1D, SlowFast and TPN) using a single command line.
 
 
+"""
+
 ######################################################################
 # Custom DataLoader
 # ------------------
@@ -32,7 +34,7 @@ You can start fine-tuning from many popular pre-trained models (e.g., I3D, R2+1D
 # The second item is the number of frames in each video. But you can put any number here
 # because our video loader will compute the number of frames again automatically during training.
 # The third item is the label of that video, e.g., 0.
-# ``val.txt`` looks the same as `train.txt`` in terms of format.
+# ``val.txt`` looks the same as ``train.txt`` in terms of format.
 #
 #
 # Once you prepare the ``train.txt`` and ``val.txt``, you are good to go.
@@ -41,8 +43,17 @@ You can start fine-tuning from many popular pre-trained models (e.g., I3D, R2+1D
 # First, prepare the data anotation files as mentioned above.
 # Second, follow this configuration file `i3d_resnet50_v1_custom.yaml <https://raw.githubusercontent.com/dmlc/gluon-cv/master/scripts/action-recognition/configuration/i3d_resnet50_v1_custom.yaml>`_.
 # Specifically, you just need to change the data paths and number of classes in that yaml file.
+#
+# ::
+#
+#     TRAIN_ANNO_PATH: '/home/ubuntu/data/sthsthv2/sthsthv2_train.txt'
+#     VAL_ANNO_PATH: '/home/ubuntu/data/sthsthv2/sthsthv2_val.txt'
+#     TRAIN_DATA_PATH: '/home/ubuntu/data/sthsthv2/20bn-something-something-v2/'
+#     VAL_DATA_PATH:  '/home/ubuntu/data/sthsthv2/20bn-something-something-v2/'
+#     NUM_CLASSES: 174
+#
 # If you want to tune other parameters, it is also easy to do.
-# Change the learning rate, batch size, clip lenght according to your use cases.
+# Change the learning rate, batch size, clip lenght according to your use cases. Usually a small learning rate is preferred since the model initialization is decent.
 
 
 ######################################################################
