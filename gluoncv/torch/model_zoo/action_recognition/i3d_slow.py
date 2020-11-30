3a8af60d2e6b8d35b3a21fa5f264ddc858809aa0
Mohammadreza Zolfaghari <mohamadreza.zolfaghari@gmail.com>
2020-12-01 00:10:31 +0100

    Update i3d_slow.py

--- gluoncv/torch/model_zoo/action_recognition/i3d_slow.py
+++ gluoncv/torch/model_zoo/action_recognition/i3d_slow.py
@@ -15,7 +15,7 @@ from .non_local import build_nonlocal_block
 __all__ = ['ResNet_SlowFast', 'i3d_slow_resnet50_f32s2_kinetics400',
            'i3d_slow_resnet50_f16s4_kinetics400', 'i3d_slow_resnet50_f8s8_kinetics400',
            'i3d_slow_resnet101_f32s2_kinetics400', 'i3d_slow_resnet101_f16s4_kinetics400',
-           'i3d_slow_resnet101_f8s8_kinetics400', 'i3d_slow_resnet50_f32s2_custom']
+           'i3d_slow_resnet101_f8s8_kinetics400', 'i3d_slow_resnet50_f32s2_custom', 'i3d_slow_resnet101_f16s4_kinetics700']
 
 
 def conv3x3x3(in_planes, out_planes, spatial_stride=1, temporal_stride=1, dilation=1):
