a796ea4a4516d95b6e1ee42ba664757d802aa234
Mohammadreza Zolfaghari <mohamadreza.zolfaghari@gmail.com>
2021-01-03 19:57:24 +0100

    update config

--- gluoncv/torch/engine/config.py
+++ gluoncv/torch/engine/config.py
@@ -162,11 +162,13 @@ _C.CONFIG.LOG.DISPLAY_FREQ = 1
 # ---------------------------------------------------------------------------- #
 
 _C.CONFIG.COOT_DATA = CN(new_allowed=True)
-
 _C.CONFIG.COOT_MODEL = CN(new_allowed=True)
-_C.CONFIG.COOT_MODEL.NAME = 'COOT-MultiModalTransformer-d384'
 _C.CONFIG.COOT_MODEL.MODEL_CONFIG = CN(new_allowed=True)
-
+_C.CONFIG.COOT_MODEL.MODEL_CONFIG.VIDEO_POOLER = CN(new_allowed=True)
+_C.CONFIG.COOT_MODEL.MODEL_CONFIG.VIDEO_SEQUENCER = CN(new_allowed=True)
+_C.CONFIG.COOT_MODEL.MODEL_CONFIG.TEXT_POOLER = CN(new_allowed=True)
+_C.CONFIG.COOT_MODEL.MODEL_CONFIG.TEXT_SEQUENCER = CN(new_allowed=True)
+_C.CONFIG.COOT_MODEL.MODEL_CONFIG.TEXT_ENCODER = CN(new_allowed=True)
 
 def get_cfg_defaults():
     """Get a yacs CfgNode object with default values for your project."""
