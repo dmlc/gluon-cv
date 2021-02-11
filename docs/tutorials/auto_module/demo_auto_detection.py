59dafd00d00992d91b1ef04c80257e0ece9b2145
Joshua Z. Zhang <cheungchih@gmail.com>
2021-02-11 15:58:25 -0800

    reduce batch size

--- docs/tutorials/auto_module/demo_auto_detection.py
+++ docs/tutorials/auto_module/demo_auto_detection.py
@@ -33,7 +33,9 @@ train, val, test = train.random_split(val_size=0.1, test_size=0.1)
 time_limits = 60 * 60  # 1hr
 search_args = {'lr': ag.Categorical(1e-3, 1e-2),
                'num_trials': 1,
-               'epochs': 2, 'num_workers': 16,
+               'epochs': 2,
+               'num_workers': 16,
+               'batch_size': ag.Categorical(4, 8),
                'ngpus_per_trial': 1,
                'search_strategy': 'random',
                'time_limits': time_limits}
