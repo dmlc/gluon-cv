bbf277fce645caa4fc45b8a55131ba9747204b5d
Joshua Z. Zhang <cheungchih@gmail.com>
2021-02-16 22:42:29 -0800

    dd

--- gluoncv/auto/tasks/image_classification.py
+++ gluoncv/auto/tasks/image_classification.py
@@ -106,6 +106,8 @@ def _train_image_classification(args, reporter):
             result = {}
             for dd in trial_dirs:
                 try:
+                    print('file', os.path.join(log_dir, dd, valid_summary_file), os.path.isfile(os.path.join(log_dir, dd, valid_summary_file)))
+                    print('ch', os.path.isfile(os.path.join(log_dir, dd, _BEST_CHECKPOINT_FILE), os.path.isfile(os.path.isfile(os.path.join(log_dir, dd, _BEST_CHECKPOINT_FILE)))
                     with open(os.path.join(log_dir, dd, valid_summary_file), 'r') as f:
                         result = json.load(f)
                         acc = results.get('valid_acc', -1)
