a753dca87f79229624ac83b02feb38e241c1085c
yinweisu <wyin@ucdavis.edu>
2021-04-05 14:26:27 -0700

    add no cache to build image

--- tools/batch/docker/docker_deploy.sh
+++ tools/batch/docker/docker_deploy.sh
@@ -8,11 +8,11 @@ if [ -z $TYPE ]; then
 fi;
 
 if [ $TYPE == cpu ] || [ $TYPE == CPU ]; then
-	docker build -f Dockerfile.cpu -t gluon-cv-1:cpu-latest .
+	docker build --no-cache -f Dockerfile.cpu -t gluon-cv-1:cpu-latest .
 	docker tag gluon-cv-1:cpu-latest $AWS_ECR_REPO:cpu-latest
 	docker push $AWS_ECR_REPO:cpu-latest
 elif [ $TYPE == gpu ] || [ $TYPE == GPU ]; then
-	docker build -f Dockerfile.gpu -t gluon-cv-1:latest .
+	docker build --no-cache -f Dockerfile.gpu -t gluon-cv-1:latest .
 	docker tag gluon-cv-1:latest $AWS_ECR_REPO:latest
 	docker push $AWS_ECR_REPO:latest
 else
