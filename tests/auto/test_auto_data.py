8adaa6b1395ec9e0468cd14bb31a968f9c545b5f
Joshua Z. Zhang <cheungchih@gmail.com>
2021-07-11 23:21:51 -0700

    fix

--- tests/auto/test_auto_data.py
+++ tests/auto/test_auto_data.py
@@ -43,6 +43,14 @@ def test_image_datasets_from_csv():
         d2 = ImageClassificationDataset(abnormal_csv, image_column='some_image', label_column='some_label')
         d2 = ImageClassificationDataset.from_csv(abnormal_csv, image_column='some_image', label_column='some_label')
 
+def test_object_detection_data():
+    df = ObjectDetection.Dataset.from_voc('https://autogluon.s3.amazonaws.com/datasets/tiny_motorbike.zip')
+    if df.is_packed():
+        df2 = df.unpack().pack()
+    else:
+        df2 = df.pack().unpack()
+    assert df2.equals(df), 'pack unpack inconsistent'
+
 if __name__ == '__main__':
     import nose
     nose.runmodule()
