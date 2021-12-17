d934e604c905531e005dcec05e137637a1da5a2e
Weisu Yin <weisuyin96@gmail.com>
2021-12-17 22:24:08 +0000

    fix lint

--- gluoncv/auto/__init__.py
+++ gluoncv/auto/__init__.py
@@ -3,4 +3,9 @@ import logging
 from .estimators import *
 
 logger = logging.getLogger(__name__)
-logger.warning("We plan to deprecate auto from gluoncv on release 0.12.0. Please consider using autogluon.vision instead, which provides the same functionality. https://auto.gluon.ai/stable/tutorials/image_prediction/index.html")
+msg = (
+    "We plan to deprecate auto from gluoncv on release 0.12.0."
+    "Please consider using autogluon.vision instead, which provides the same functionality."
+    "https://auto.gluon.ai/stable/tutorials/image_prediction/index.html"
+)
+logger.warning(msg)
